4db3065835b43be63887c3a9caa44661303424b0
Zerom999 <maregee@hotmail.com>
2021-07-19 13:42:55 -0230

    Update MidTank.asm

--- Units/AW2/MidTank.asm
+++ Units/AW2/MidTank.asm
@@ -25,7 +25,7 @@
 	setUnitDamagePrimary		Unit_MidTank,30,Unit_Infantry
 	setUnitDamagePrimary		Unit_MidTank,30,Unit_Mech
 	
-	setUnitDamagePrimary		Unit_MidTank,75,Unit_Tank
+	setUnitDamagePrimary		Unit_MidTank,85,Unit_Tank
 	setUnitDamagePrimary		Unit_MidTank,55,Unit_MidTank
 	setUnitDamagePrimary		Unit_MidTank,35,Unit_Neotank
 	
@@ -37,25 +37,25 @@
 	setUnitDamagePrimary		Unit_MidTank,105,Unit_APC
 	setUnitDamagePrimary		Unit_MidTank,105,Unit_AntiAir
 
-	setUnitDamagePrimary		Unit_MidTank,10,Unit_Battleship
+	setUnitDamagePrimary		Unit_MidTank,15,Unit_Battleship
 	setUnitDamagePrimary		Unit_MidTank,45,Unit_Cruiser
 	setUnitDamagePrimary		Unit_MidTank,35,Unit_Lander
-	setUnitDamagePrimary		Unit_MidTank,10,Unit_Sub
+	setUnitDamagePrimary		Unit_MidTank,15,Unit_Sub
 
 	setUnitDamageSecondary		Unit_MidTank,105,Unit_Infantry
 	setUnitDamageSecondary		Unit_MidTank,95,Unit_Mech
 	
-	setUnitDamageSecondary		Unit_MidTank,08,Unit_Tank
-	setUnitDamageSecondary		Unit_MidTank,01,Unit_MidTank
+	setUnitDamageSecondary		Unit_MidTank,15,Unit_Tank
+	setUnitDamageSecondary		Unit_MidTank,05,Unit_MidTank
 	setUnitDamageSecondary		Unit_MidTank,01,Unit_Neotank
 	
-	setUnitDamageSecondary		Unit_MidTank,45,Unit_Artillery
+	setUnitDamageSecondary		Unit_MidTank,55,Unit_Artillery
 	setUnitDamageSecondary		Unit_MidTank,55,Unit_Rockets
-	setUnitDamageSecondary		Unit_MidTank,35,Unit_Missiles
+	setUnitDamageSecondary		Unit_MidTank,55,Unit_Missiles
 
-	setUnitDamageSecondary		Unit_MidTank,45,Unit_Recon
+	setUnitDamageSecondary		Unit_MidTank,55,Unit_Recon
 	setUnitDamageSecondary		Unit_MidTank,45,Unit_APC
-	setUnitDamageSecondary		Unit_MidTank,07,Unit_AntiAir
+	setUnitDamageSecondary		Unit_MidTank,35,Unit_AntiAir
 
 	setUnitDamageSecondary		Unit_MidTank,12,Unit_Battlecopter
 	setUnitDamageSecondary		Unit_MidTank,45,Unit_TransportCopter
