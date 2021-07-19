e8c1abdcc128e560dc028fb9ea8b2f68420467cb
Zerom999 <maregee@hotmail.com>
2021-07-18 22:42:45 -0230

    Update Eagle.asm

--- COs/AWDS/Eagle.asm
+++ COs/AWDS/Eagle.asm
@@ -21,23 +21,23 @@
 	setCOFaction			coAWDSEagle,ArmyGreenEarth
 	setCOMusic          	coAWDSEagle,MusicEagleTheme
 
-	setCOPowerCost          coAWDSEagle,3
-	setCOSuperCost          coAWDSEagle,9
+	setCOPowerCost          coAWDSEagle,5
+	setCOSuperCost          coAWDSEagle,10
 	
 	setCOD2DAbilities	coAWDSEagle,SpecialAerialFuelSaver
 	setCOPowerAbilities	coAWDSEagle,SpecialAerialFuelSaver
 	setCOSuperAbilities	coAWDSEagle,SpecialAerialFuelSaver
 
-	setCOD2DNavyStat	coAWDSEagle,-10,0,0,0
-	setCOD2DAirStat		coAWDSEagle,20,0,0,0
+	setCOD2DNavyStat	coAWDSEagle,-40,0,0,0
+	setCOD2DAirStat		coAWDSEagle,20,20,0,0
 
-	setCOPowerAllStat	coAWDSEagle,-50,0,0,0
-	setCOPowerNavyStat	coAWDSEagle,-55,0,0,0
-	setCOPowerAirStat	coAWDSEagle,-40,0,0,0
+	setCOPowerAllStat	coAWDSEagle,0,0,0,0
+	setCOPowerNavyStat	coAWDSEagle,0,0,0,0
+	setCOPowerAirStat	coAWDSEagle,0,0,0,0
 	setCOPowerSoldierStat	coAWDSEagle,0,0,0,0
 
-	setCOSuperAirStat	coAWDSEagle,20,0,0,0
-	setCOSuperNavyStat	coAWDSEagle,-10,0,0,0
+	setCOSuperAirStat	coAWDSEagle,60,0,0,0
+	setCOSuperNavyStat	coAWDSEagle,-40,0,0,0
 
 	setCOGFXPowerRule	coAWDSEagle,COPCheckAerialClassOrWaitMode
 	setCOGFXPowerFX		coAWDSEagle,COPEffectReanimateUnit
