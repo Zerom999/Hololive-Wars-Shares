6a08424ae9bcdb205503d0efe986a5c5b4907294
Zerom999 <maregee@hotmail.com>
2021-07-19 13:44:38 -0230

    Update Infantry.asm

--- Units/AW2/Infantry.asm
+++ Units/AW2/Infantry.asm
@@ -22,20 +22,20 @@
 	setUnitDamageSecondary	Unit_Infantry,55,Unit_Infantry
 	setUnitDamageSecondary	Unit_Infantry,45,Unit_Mech
 	
-	setUnitDamageSecondary	Unit_Infantry,05,Unit_Tank
+	setUnitDamageSecondary	Unit_Infantry,01,Unit_Tank
 	setUnitDamageSecondary	Unit_Infantry,01,Unit_MidTank
 	setUnitDamageSecondary	Unit_Infantry,01,Unit_Neotank
 	
 	setUnitDamageSecondary	Unit_Infantry,15,Unit_Artillery
-	setUnitDamageSecondary	Unit_Infantry,25,Unit_Rockets
-	setUnitDamageSecondary	Unit_Infantry,25,Unit_Missiles
+	setUnitDamageSecondary	Unit_Infantry,15,Unit_Rockets
+	setUnitDamageSecondary	Unit_Infantry,15,Unit_Missiles
 	
-	setUnitDamageSecondary	Unit_Infantry,12,Unit_Recon
-	setUnitDamageSecondary	Unit_Infantry,14,Unit_APC
-	setUnitDamageSecondary	Unit_Infantry,05,Unit_AntiAir
+	setUnitDamageSecondary	Unit_Infantry,05,Unit_Recon
+	setUnitDamageSecondary	Unit_Infantry,15,Unit_APC
+	setUnitDamageSecondary	Unit_Infantry,01,Unit_AntiAir
 	
-	setUnitDamageSecondary	Unit_Infantry,7,Unit_BattleCopter
-	setUnitDamageSecondary	Unit_Infantry,30,Unit_TransportCopter
+	setUnitDamageSecondary	Unit_Infantry,5,Unit_BattleCopter
+	setUnitDamageSecondary	Unit_Infantry,15,Unit_TransportCopter
 
 	setUnitInventionDamage	Unit_Infantry,00,01
 	
