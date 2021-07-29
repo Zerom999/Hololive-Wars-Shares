42987929b336c61a95ef588223630ff48986083b
Zerom999 <maregee@hotmail.com>
2021-07-28 22:46:00 -0230

    Update Kanata.asm

--- COs/AWDS/Kanata.asm
+++ COs/AWDS/Kanata.asm
@@ -1,30 +1,30 @@
 	setCODefaults           coAWDSKanata
 	setCOGameID		coAWDSKanata,Game_AWDS
 
-	setCOGFXFullbody        coAWDSKanata,GraphicsAWDSAndyGraphic
-	setCOGFXName            coAWDSKanata,GraphicsAWDSAndyName
-	setCOGFXPalette         coAWDSKanata,GraphicsAWDSAndyPalette
-	setCOGFXFace            coAWDSKanata,GraphicsAWDSAndyFace,GraphicsAWDSAndyHappyFace,GraphicsAWDSAndySadFace
-	setCOGFXSmallFace       coAWDSKanata,GraphicsAWDSAndyMugshot
+	setCOGFXFullbody        coAWDSKanata,GraphicsAWDSKanataGraphic
+	setCOGFXName            coAWDSKanata,GraphicsAWDSKanataName
+	setCOGFXPalette         coAWDSKanata,GraphicsAWDSKanataPalette
+	setCOGFXFace            coAWDSKanata,GraphicsAWDSKanataFace,GraphicsAWDSKanataHappyFace,GraphicsAWDSKanataSadFace
+	setCOGFXSmallFace       coAWDSKanata,GraphicsAWDSKanataMugshot
 	setCOGFXPowerAnim       coAWDSKanata,5,0
 	setCOGFXPowerSFX        coAWDSAndy,0xb4,0xb5
 	setCOGFXSuperAnim       coAWDSAndy,7,7
 	setCOGFXSuperSFX        coAWDSAndy,0x1f7,0x1f8
 	
-	setCOName               coAWDSAndy,ScriptAWDSAndy
-	setCOpowerName          coAWDSAndy,ScriptAWDSAndy+1
-	setCOsuperName          coAWDSAndy,ScriptAWDSAndy+2
-	setCOpowerText          coAWDSAndy,ScriptAWDSAndy+8,ScriptAWDSAndy+9,ScriptAWDSAndy+10,ScriptAWDSAndy+11,ScriptAWDSAndy+12,ScriptAWDSAndy+13
-	setCOprofileText        coAWDSAndy,ScriptAWDSAndy+3,ScriptAWDSAndy+4,ScriptAWDSAndy+5,ScriptAWDSAndy+6
-	setCOvictoryText        coAWDSAndy,ScriptAWDSAndy+7
+	setCOName               coAWDSAndy,ScriptAWDSKanata
+	setCOpowerName          coAWDSAndy,ScriptAWDSKanata+1
+	setCOsuperName          coAWDSAndy,ScriptAWDSKanata+2
+	setCOpowerText          coAWDSAndy,ScriptAWDSKanata+8,ScriptAWDSKanata+9,ScriptAWDSKanata+10,ScriptAWDSAndy+11,ScriptAWDSKanata+12,ScriptAWDSKanata+13
+	setCOprofileText        coAWDSAndy,ScriptAWDSKanata+3,ScriptAWDSKanata+4,ScriptAWDSKanata+5,ScriptAWDSKanata+6
+	setCOvictoryText        coAWDSAndy,ScriptAWDSKanata+7
 
 	setCOFaction		coAWDSAndy,ArmyOrangeStar
-	setCOMusic          	coAWDSAndy,MusicAndyTheme
+	setCOMusic          	coAWDSAndy,MusicKanataTheme
 
 	setCOPowerCost          coAWDSAndy,3
 	setCOSuperCost          coAWDSAndy,6
 
-	setCOSuperAllStat	coAWDSAndy,30,0,1,0
+	setCOSuperAllStat	coAWDSAndy,30,10,1,0
 
 	setCOGFXPowerFX         coAWDSandy,COPEffectHealThreeHP
 	setCOGFXSuperFX         coAWDSandy,COPEffectHealSixHP
