1df2a391bf11de70b01e44e8c5afd6401d866f07
Zerom999 <maregee@hotmail.com>
2021-07-28 22:58:05 -0230

    Create Fubuki.asm

---
+++ COs/AWDS/Fubuki.asm
@@ -0,0 +1,37 @@
+setCODefaults           coAWDSOlaf
+	setCOGameID		coAWDSOlaf,Game_AWDS
+
+	setCOGFXFullbody        coAWDSOlaf,GraphicsAWDSOlafGraphic
+	setCOGFXName            coAWDSOlaf,GraphicsAWDSOlafName
+	setCOGFXPalette         coAWDSOlaf,GraphicsAWDSOlafPalette
+	setCOGFXFace            coAWDSOlaf,GraphicsAWDSOlafFace,GraphicsAWDSOlafHappyFace,GraphicsAWDSOlafSadFace
+	setCOGFXSmallFace       coAWDSOlaf,GraphicsAWDSOlafMugshot
+	setCOGFXPowerAnim       coAWDSOlaf,0,0
+	setCOGFXPowerSFX        coAWDSOlaf,0xC3,0
+	setCOGFXSuperAnim       coAWDSOlaf,7,7
+	setCOGFXSuperSFX        coAWDSOlaf,0x1f7,0x1f8
+	
+	setCOName               coAWDSOlaf,ScriptAWDSOlaf
+	setCOpowerName          coAWDSOlaf,ScriptAWDSOlaf+1
+	setCOsuperName          coAWDSOlaf,ScriptAWDSOlaf+2
+	setCOpowerText          coAWDSOlaf,ScriptAWDSOlaf+8,ScriptAWDSOlaf+9,ScriptAWDSOlaf+10,ScriptAWDSOlaf+11,ScriptAWDSOlaf+12,ScriptAWDSOlaf+13
+	setCOprofileText        coAWDSOlaf,ScriptAWDSOlaf+3,ScriptAWDSOlaf+4,ScriptAWDSOlaf+5,ScriptAWDSOlaf+6
+	setCOvictoryText        coAWDSOlaf,ScriptAWDSOlaf+7
+
+	setCOFaction		coAWDSOlaf,ArmyBlueMoon
+	setCOMusic          	coAWDSOlaf,MusicOlafTheme
+
+	setCOPowerCost          coAWDSOlaf,4
+	setCOSuperCost          coAWDSOlaf,8
+
+	setCOD2DSnowFirepower	coAWDSOlaf,40
+	setCOPowerSnowFirepower	coAWDSOlaf,40
+	setCOSuperSnowFirepower	coAWDSOlaf,40
+
+	setCOPowerASM		coAWDSOlaf,COPowerExtendedBlizzard
+	setCOSuperASM		coAWDSOlaf,COPowerExtendedWinterFury
+	
+	setCOAIPowerRange       coAWDSOlaf,15
+	setCOAIPowerTrigger     coAWDSOlaf,70
+	setCOAIPowerTriggerASM	coAWDSOlaf,AIActivateNotSnow
+; blank line
