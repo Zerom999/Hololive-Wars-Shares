a35ac1f45c03088a9743e72ecbfcf96ceaa7ea6d
Zerom999 <maregee@hotmail.com>
2021-07-28 22:51:01 -0230

    Update Kanata.asm

--- COs/AWDS/Kanata.asm
+++ COs/AWDS/Kanata.asm
@@ -11,15 +11,15 @@
 	setCOGFXSuperAnim       coAWDSAndy,7,7
 	setCOGFXSuperSFX        coAWDSAndy,0x1f7,0x1f8
 	
-	setCOName               coAWDSAndy,ScriptAWDSKanata
-	setCOpowerName          coAWDSAndy,ScriptAWDSKanata+1
-	setCOsuperName          coAWDSAndy,ScriptAWDSKanata+2
-	setCOpowerText          coAWDSAndy,ScriptAWDSKanata+8,ScriptAWDSKanata+9,ScriptAWDSKanata+10,ScriptAWDSAndy+11,ScriptAWDSKanata+12,ScriptAWDSKanata+13
-	setCOprofileText        coAWDSAndy,ScriptAWDSKanata+3,ScriptAWDSKanata+4,ScriptAWDSKanata+5,ScriptAWDSKanata+6
-	setCOvictoryText        coAWDSAndy,ScriptAWDSKanata+7
+	setCOName               coAWDSKanata,ScriptAWDSKanata
+	setCOpowerName          coAWDSKanata,ScriptAWDSKanata+1
+	setCOsuperName          coAWDSKanata,ScriptAWDSKanata+2
+	setCOpowerText          coAWDSKanata,ScriptAWDSKanata+8,ScriptAWDSKanata+9,ScriptAWDSKanata+10,ScriptAWDSKanata+11,ScriptAWDSKanata+12,ScriptAWDSKanata+13
+	setCOprofileText        coAWDSKanata,ScriptAWDSKanata+3,ScriptAWDSKanata+4,ScriptAWDSKanata+5,ScriptAWDSKanata+6
+	setCOvictoryText        coAWDSKanata,ScriptAWDSKanata+7
 
-	setCOFaction		coAWDSAndy,ArmyOrangeStar
-	setCOMusic          	coAWDSAndy,MusicKanataTheme
+	setCOFaction		coAWDSKanata,ArmyOrangeStar
+	setCOMusic          	coAWDSKanata,MusicKanataTheme
 
 	setCOPowerCost          coAWDSAndy,3
 	setCOSuperCost          coAWDSAndy,6
