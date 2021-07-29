fac54dcca0f7074352aae09aaaaac7903e2a9ff8
Zerom999 <maregee@hotmail.com>
2021-07-29 15:37:41 -0230

    Update Aqua.asm

--- COs/AWDS/Aqua.asm
+++ COs/AWDS/Aqua.asm
@@ -14,15 +14,15 @@
 	setCOName               coAWDSAqua,ScriptAWDSAqua
 	setCOpowerName          coAWDSAqua,ScriptAWDSAqua+1
 	setCOsuperName          coAWDSAqua,ScriptAWDSAqua+2
-	setCOpowerText          coAWDSAqua,ScriptAWDSAqua+8,ScriptAWDSAqua+9,ScriptAWDSJess+10,ScriptAWDSJess+11,ScriptAWDSJess+12,ScriptAWDSJess+13
-	setCOprofileText        coAWDSAqua,ScriptAWDSAqua+3,ScriptAWDSAqua+4,ScriptAWDSJess+5,ScriptAWDSJess+6
+	setCOpowerText          coAWDSAqua,ScriptAWDSAqua+8,ScriptAWDSAqua+9,ScriptAWDSAqua+10,ScriptAWDSAqua+11,ScriptAWDSAqua+12,ScriptAWDSAqua+13
+	setCOprofileText        coAWDSAqua,ScriptAWDSAqua+3,ScriptAWDSAqua+4,ScriptAWDSAqua+5,ScriptAWDSAqua+6
 	setCOvictoryText        coAWDSAqua,ScriptAWDSAqua+7
 
-	setCOFaction		coAWDSJess,ArmyGreenEarth
-	setCOMusic          	coAWDSJess,MusicJessTheme
+	setCOFaction		coAWDSAqua,ArmyGreenEarth
+	setCOMusic          	coAWDSAqua,MusicAquaTheme
 
-	setCOPowerCost          coAWDSJess,3
-	setCOSuperCost          coAWDSJess,6
+	setCOPowerCost          coAWDSAqua,3
+	setCOSuperCost          coAWDSAqua,6
 	
 	setCOD2DLandStat	coAWDSJess,0,0,0,0
 	setCOD2DNavyStat	coAWDSJess,0,30,1,0
