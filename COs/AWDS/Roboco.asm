3eaa66f4f117e69f84614d95f3306a23cbbcf190
Zerom999 <maregee@hotmail.com>
2021-07-29 20:15:31 -0230

    Update Roboco.asm

--- COs/AWDS/Roboco.asm
+++ COs/AWDS/Roboco.asm
@@ -1,27 +1,27 @@
 	setCODefaults           coAWDSRoboco
 	setCOGameID		coAWDSRoboco,Game_AWDS
 
-	setCOGFXFullbody        coAWDSRoboco,GraphicsAWDSKindleGraphic
-	setCOGFXName            coAWDSRoboco,GraphicsAWDSKindleName
-	setCOGFXPalette         coAWDSRoboco,GraphicsAWDSKindlePalette
-	setCOGFXFace            coAWDSRoboco,GraphicsAWDSKindleFace,GraphicsAWDSKindleHappyFace,GraphicsAWDSKindleSadFace
-	setCOGFXSmallFace       coAWDSRoboco,GraphicsAWDSKindleMugshot
+	setCOGFXFullbody        coAWDSRoboco,GraphicsAWDSRobocoGraphic
+	setCOGFXName            coAWDSRoboco,GraphicsAWDSRobocoName
+	setCOGFXPalette         coAWDSRoboco,GraphicsAWDSRobocoPalette
+	setCOGFXFace            coAWDSRoboco,GraphicsAWDSRobocoFace,GraphicsAWDSRobocoHappyFace,GraphicsAWDSRobocoSadFace
+	setCOGFXSmallFace       coAWDSRoboco,GraphicsAWDSRobocoMugshot
 	setCOGFXPowerAnim       coAWDSRoboco,2,0
 	setCOGFXPowerSFX        coAWDSRoboco,0xb7,0xb8
 	setCOGFXSuperAnim       coAWDSRoboco,7,7
 	setCOGFXSuperSFX        coAWDSRoboco,0x1F4,0x1f5
 	
-	setCOName               coAWDSKindle,ScriptAWDSKindle
-	setCOpowerName          coAWDSKindle,ScriptAWDSKindle+1
-	setCOsuperName          coAWDSKindle,ScriptAWDSKindle+2
-	setCOpowerText          coAWDSKindle,ScriptAWDSKindle+8,ScriptAWDSKindle+9,ScriptAWDSKindle+10,ScriptAWDSKindle+11,ScriptAWDSKindle+12,ScriptAWDSKindle+13
-	setCOprofileText        coAWDSKindle,ScriptAWDSKindle+3,ScriptAWDSKindle+4,ScriptAWDSKindle+5,ScriptAWDSKindle+6
-	setCOvictoryText        coAWDSKindle,ScriptAWDSKindle+7
+	setCOName               coAWDSRoboco,ScriptAWDSKindle
+	setCOpowerName          coAWDSRoboco,ScriptAWDSKindle+1
+	setCOsuperName          coAWDSRoboco,ScriptAWDSKindle+2
+	setCOpowerText          coAWDSRoboco,ScriptAWDSKindle+8,ScriptAWDSKindle+9,ScriptAWDSKindle+10,ScriptAWDSKindle+11,ScriptAWDSKindle+12,ScriptAWDSKindle+13
+	setCOprofileText        coAWDSRoboco,ScriptAWDSKindle+3,ScriptAWDSKindle+4,ScriptAWDSKindle+5,ScriptAWDSKindle+6
+	setCOvictoryText        coAWDSRoboco,ScriptAWDSKindle+7
 
-	setCOFaction		coAWDSKindle,ArmyBlackHole
-	setCOMusic          	coAWDSKindle,MusicBlackHole
-	setCOPowerMusic		coAWDSKindle,MusicBlackPower
-	setCOSuperMusic		coAWDSKindle,MusicBlackSuper
+	setCOFaction		coAWDSRoboco,ArmyBlackHole
+	setCOMusic          	coAWDSRoboco,MusicBlackHole
+	setCOPowerMusic		coAWDSRoboco,MusicBlackPower
+	setCOSuperMusic		coAWDSRoboco,MusicBlackSuper
 
 	setCOPowerCost		coAWDSKindle,3
 	setCOSuperCost		coAWDSKindle,6
