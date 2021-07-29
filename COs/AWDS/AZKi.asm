80e687fd2d12d320cb589dd4940b5e100ab8e78f
Zerom999 <maregee@hotmail.com>
2021-07-29 19:25:03 -0230

    Update AZKi.asm

--- COs/AWDS/AZKi.asm
+++ COs/AWDS/AZKi.asm
@@ -1,22 +1,22 @@
 	setCODefaults           coAWDSAZKi
 	setCOGameID		coAWDSAZKi,Game_AWDS
 
-	setCOGFXFullbody        coAWDSAZKi,GraphicsAWDSJavierGraphic
-	setCOGFXName            coAWDSAZKi,GraphicsAWDSJavierName
-	setCOGFXPalette         coAWDSAZKi,GraphicsAWDSJavierPalette
-	setCOGFXFace            coAWDSAZKi,GraphicsAWDSJavierFace,GraphicsAWDSJavierHappyFace,GraphicsAWDSJavierSadFace
-	setCOGFXSmallFace       coAWDSAZKi,GraphicsAWDSJavierMugshot
+	setCOGFXFullbody        coAWDSAZKi,GraphicsAWDSAZKiGraphic
+	setCOGFXName            coAWDSAZKi,GraphicsAWDSAZKiName
+	setCOGFXPalette         coAWDSAZKi,GraphicsAWDSAZKiPalette
+	setCOGFXFace            coAWDSAZKi,GraphicsAWDSAZKiFace,GraphicsAWDSAZKiHappyFace,GraphicsAWDSAZKiSadFace
+	setCOGFXSmallFace       coAWDSAZKi,GraphicsAWDSAZKiMugshot
 	setCOGFXPowerAnim       coAWDSAZKi,3,1
 	setCOGFXPowerSFX        coAWDSAZKi,0xb7,0xb8
 	setCOGFXSuperAnim       coAWDSAZKi,7,7
 	setCOGFXSuperSFX        coAWDSAZKi,0x1f7,0x1f8
 	
-	setCOName               coAWDSJavier,ScriptAWDSJavier
-	setCOpowerName          coAWDSJavier,ScriptAWDSJavier+1
-	setCOsuperName          coAWDSJavier,ScriptAWDSJavier+2
-	setCOpowerText          coAWDSJavier,ScriptAWDSJavier+8,ScriptAWDSJavier+9,ScriptAWDSJavier+10,ScriptAWDSJavier+11,ScriptAWDSJavier+12,ScriptAWDSJavier+13
-	setCOprofileText        coAWDSJavier,ScriptAWDSJavier+3,ScriptAWDSJavier+4,ScriptAWDSJavier+5,ScriptAWDSJavier+6
-	setCOvictoryText        coAWDSJavier,ScriptAWDSJavier+7
+	setCOName               coAWDSAZKi,ScriptAWDSAZKi
+	setCOpowerName          coAWDSAZKi,ScriptAWDSAZKi+1
+	setCOsuperName          coAWDSAZKi,ScriptAWDSJavier+2
+	setCOpowerText          coAWDSAZKi,ScriptAWDSJavier+8,ScriptAWDSJavier+9,ScriptAWDSJavier+10,ScriptAWDSJavier+11,ScriptAWDSJavier+12,ScriptAWDSJavier+13
+	setCOprofileText        coAWDSAZKi,ScriptAWDSJavier+3,ScriptAWDSJavier+4,ScriptAWDSJavier+5,ScriptAWDSJavier+6
+	setCOvictoryText        coAWDSAZKi,ScriptAWDSJavier+7
 
 	setCOFaction		coAWDSJavier,ArmyGreenEarth
 	setCOMusic          	coAWDSJavier,MusicGreenEarth
