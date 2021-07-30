2c1c41e11a780b8a451badb50dade105a1fc469c
Zerom999 <maregee@hotmail.com>
2021-07-30 18:54:34 -0230

    Update terrainboosterkindlepower.asm

--- datatables/COStats/TerrainBoosters/terrainboosterkindlepower.asm
+++ datatables/COStats/TerrainBoosters/terrainboosterkindlepower.asm
@@ -6,15 +6,15 @@ TerrainFirepowerBoostKindlePower:
 	;Null, Plains, River, Mountain
 	;.db 0,0,0,0
 	;Forest, Road, City, Sea
-	;.db 0,0,81,0
+	;.db 0,0,100,0
 	;HQ, None, Airport, Port
-	;.db 81,0,81,81
+	;.db 100,0,100,100
 	;Bridge, Shoal, Base, Pipeline
-	;.db 0,0,81,0
+	;.db 0,0,100,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	;.db 0,0,0,0
+	;.db 0,100,100,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
-	;.db 81,0,0,0
+	;.db 100,0,0,0
 	;MinicannonE, Laser, BlackCannonS, BlackCannonN
 	;.db 0,0,0,0
 	;Volcano, Factory, Deathray, Variable Tile
@@ -23,11 +23,11 @@ TerrainFirepowerBoostKindlePower:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,0,81,0
+	.db 0,0,100,0
 	;HQ, None, Airport, Port
-	.db 81,0,81,81
+	.db 81,0,100,100
 	;Bridge, Shoal, Base, Pipeline
-	.db 0,0,81,0
+	.db 0,0,100,0
 	;Pipeseam, Silo, Silo - Launched, Reef
 	.db 0,0,0,0
 	;Lab, Minicannon, Tower, None
