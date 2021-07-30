2fb9c36e8a07f55673ab4dd4a426eb3bcc4c9138
Zerom999 <maregee@hotmail.com>
2021-07-30 19:06:44 -0230

    Update terrainboosterkindledaily.asm

--- datatables/COStats/TerrainBoosters/terrainboosterkindledaily.asm
+++ datatables/COStats/TerrainBoosters/terrainboosterkindledaily.asm
@@ -12,7 +12,7 @@ TerrainFirepowerBoostKindleDaytoDay:
 	;Bridge, Shoal, Base, Pipeline
 	;.db 0,0,50,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	;.db 0,0,0,0
+	;.db 0,50,50,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
 	;.db 50,0,0,0
 	;MinicannonE, Laser, BlackCannonS, BlackCannonN
@@ -23,15 +23,15 @@ TerrainFirepowerBoostKindleDaytoDay:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,0,41,0
+	.db 0,0,50,0
 	;HQ, None, Airport, Port
-	.db 41,0,41,41
+	.db 50,0,50,50
 	;Bridge, Shoal, Base, Pipeline
-	.db 0,0,41,0
+	.db 0,0,50,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	.db 0,0,0,0
+	.db 0,50,50,0
 	;Lab, Minicannon, Tower, None
-	.db 41,0,41,0
+	.db 50,0,50,0
 	;None, Laser, BlackCannonS, BlackCannonN
 	.db 0,0,0,0
 	;Volcano, Factory, Deathray, Variable Tile
