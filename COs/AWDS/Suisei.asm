ffe29e32cc9bf38c41ba0dd2e5eb87dac4457933
Zerom999 <maregee@hotmail.com>
2021-07-31 19:09:52 -0230

    Update Suisei.asm

--- COs/AWDS/Suisei.asm
+++ COs/AWDS/Suisei.asm
@@ -11,18 +11,18 @@
 	setCOGFXSuperAnim       coAWDSSuisei,7,7
 	setCOGFXSuperSFX        coAWDSSuisei,0x1f7,0x1f8
 	
-	setCOName               coAWDSSuisei,ScriptAWDSGrimm
-	setCOpowerName          coAWDSSuisei,ScriptAWDSGrimm+1
-	setCOsuperName          coAWDSSuisei,ScriptAWDSGrimm+2
-	setCOpowerText          coAWDSSuisei,ScriptAWDSGrimm+8,ScriptAWDSGrimm+9,ScriptAWDSGrimm+10,ScriptAWDSGrimm+11,ScriptAWDSGrimm+12,ScriptAWDSGrimm+13
-	setCOprofileText        coAWDSSuisei,ScriptAWDSGrimm+3,ScriptAWDSGrimm+4,ScriptAWDSGrimm+5,ScriptAWDSGrimm+6
-	setCOvictoryText        coAWDSSuisei,ScriptAWDSGrimm+7
+	setCOName               coAWDSSuisei,ScriptAWDSSuisei
+	setCOpowerName          coAWDSSuisei,ScriptAWDSSuisei+1
+	setCOsuperName          coAWDSSuisei,ScriptAWDSSuisei+2
+	setCOpowerText          coAWDSSuisei,ScriptAWDSSuisei+8,ScriptAWDSGrimm+9,ScriptAWDSGrimm+10,ScriptAWDSGrimm+11,ScriptAWDSGrimm+12,ScriptAWDSGrimm+13
+	setCOprofileText        coAWDSSuisei,ScriptAWDSSuisei+3,ScriptAWDSGrimm+4,ScriptAWDSGrimm+5,ScriptAWDSGrimm+6
+	setCOvictoryText        coAWDSSuisei,ScriptAWDSSuisei+7
 
-	setCOFaction		coAWDSGrimm,ArmyYellowComet
-	setCOMusic          	coAWDSGrimm,MusicYellowComet
+	setCOFaction		coAWDSSuisei,ArmyYellowComet
+	setCOMusic          	coAWDSSuisei,MusicSuiseiComet
 
-	setCOPowerCost          coAWDSGrimm,3
-	setCOSuperCost          coAWDSGrimm,6
+	setCOPowerCost          coAWDSSuisei,5
+	setCOSuperCost          coAWDSSuisei,10
 
 	setCOD2DAllStat		coAWDSGrimm,40,-20,0,0
 	setCOPowerAllStat	coAWDSGrimm,70,-20,0,0
