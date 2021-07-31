4d8794997bc6166fff5fb6c726e610b2e284d96f
Zerom999 <maregee@hotmail.com>
2021-07-31 17:42:28 -0230

    Update Miko.asm

--- COs/AWDS/Miko.asm
+++ COs/AWDS/Miko.asm
@@ -1,22 +1,22 @@
 	setCODefaults           coAWDSMiko
 	setCOGameID		coAWDSMiko,Game_AWDS
 
-	setCOGFXFullbody        coAWDSMiko,GraphicsAWDSNellGraphic
-	setCOGFXName            coAWDSMiko,GraphicsAWDSNellName
-	setCOGFXPalette         coAWDSMiko,GraphicsAWDSNellPalette
-	setCOGFXFace            coAWDSMiko,GraphicsAWDSNellFace,GraphicsAWDSNellHappyFace,GraphicsAWDSNellSadFace
-	setCOGFXSmallFace       coAWDSMiko,GraphicsAWDSNellMugshot
+	setCOGFXFullbody        coAWDSMiko,GraphicsAWDSMikoGraphic
+	setCOGFXName            coAWDSMiko,GraphicsAWDSNMikoame
+	setCOGFXPalette         coAWDSMiko,GraphicsAWDSMikoPalette
+	setCOGFXFace            coAWDSMiko,GraphicsAWDSMikoFace,GraphicsAWDSMikoHappyFace,GraphicsAWDSMikoSadFace
+	setCOGFXSmallFace       coAWDSMiko,GraphicsAWDSMikoMugshot
 	setCOGFXPowerAnim       coAWDSMiko,2,0
 	setCOGFXPowerSFX        coAWDSMiko,0xb7,0xb8
 	setCOGFXSuperAnim       coAWDSMiko,7,7
 	setCOGFXSuperSFX        coAWDSMiko,0x1f7,0x1f8
 	
-	setCOName               coAWDSNell,ScriptAWDSNell
-	setCOpowerName          coAWDSNell,ScriptAWDSNell+1
-	setCOsuperName          coAWDSNell,ScriptAWDSNell+2
-	setCOpowerText          coAWDSNell,ScriptAWDSNell+8,ScriptAWDSNell+9,ScriptAWDSNell+10,ScriptAWDSNell+11,ScriptAWDSNell+12,ScriptAWDSNell+13
-	setCOprofileText        coAWDSNell,ScriptAWDSNell+3,ScriptAWDSNell+4,ScriptAWDSNell+5,ScriptAWDSNell+6
-	setCOvictoryText        coAWDSNell,ScriptAWDSNell+7
+	setCOName               coAWDSMiko,ScriptAWDSNell
+	setCOpowerName          coAWDSMiko,ScriptAWDSNell+1
+	setCOsuperName          coAWDSMiko,ScriptAWDSNell+2
+	setCOpowerText          coAWDSMiko,ScriptAWDSNell+8,ScriptAWDSNell+9,ScriptAWDSNell+10,ScriptAWDSNell+11,ScriptAWDSNell+12,ScriptAWDSNell+13
+	setCOprofileText        coAWDSMiko,ScriptAWDSNell+3,ScriptAWDSNell+4,ScriptAWDSNell+5,ScriptAWDSNell+6
+	setCOvictoryText        coAWDSMiko,ScriptAWDSNell+7
 
 	setCOFaction		coAWDSNell,ArmyOrangeStar
 	setCOMusic          	coAWDSNell,MusicNellTheme
