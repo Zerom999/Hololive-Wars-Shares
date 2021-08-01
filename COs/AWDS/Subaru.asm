9e2ef235ac9fa2fc2c9c5d2f33797b71c2f92610
Zerom999 <maregee@hotmail.com>
2021-08-01 19:58:17 -0230

    Update Subaru.asm

--- COs/AWDS/Subaru.asm
+++ COs/AWDS/Subaru.asm
@@ -2,21 +2,21 @@
 	setCOGameID		coAWDSSubaru,Game_AWDS
 
 	setCOGFXFullbody        coAWDSSubaru,GraphicsAWDSSubaruGraphic
-	setCOGFXName            coAWDSSubaru,GraphicsAWDSAquaName
-	setCOGFXPalette         coAWDSSubaru,GraphicsAWDSAquaPalette
-	setCOGFXFace            coAWDSSubaru,GraphicsAWDSAquaFace,GraphicsAWDSAquaHappyFace,GraphicsAWDSAquaSadFace
-	setCOGFXSmallFace       coAWDSSubaru,GraphicsAWDSAquaMugshot
+	setCOGFXName            coAWDSSubaru,GraphicsAWDSSubaruName
+	setCOGFXPalette         coAWDSSubaru,GraphicsAWDSSubaruPalette
+	setCOGFXFace            coAWDSSubaru,GraphicsAWDSSubaruFace,GraphicsAWDSSubaruHappyFace,GraphicsAWDSSubaruSadFace
+	setCOGFXSmallFace       coAWDSSubaru,GraphicsAWDSSubaruMugshot
 	setCOGFXPowerAnim       coAWDSSubaru,0x04,0x00
 	setCOGFXPowerSFX        coAWDSSubaru,0xba,0xbb
 	setCOGFXSuperAnim       coAWDSSubaru,7,7
 	setCOGFXSuperSFX        coAWDSSubaru,0x1f7,0x1f8
 	
-	setCOName               coAWDSAqua,ScriptAWDSAqua
-	setCOpowerName          coAWDSAqua,ScriptAWDSAqua+1
-	setCOsuperName          coAWDSAqua,ScriptAWDSAqua+2
-	setCOpowerText          coAWDSAqua,ScriptAWDSAqua+8,ScriptAWDSAqua+9,ScriptAWDSAqua+10,ScriptAWDSAqua+11,ScriptAWDSAqua+12,ScriptAWDSAqua+13
-	setCOprofileText        coAWDSAqua,ScriptAWDSAqua+3,ScriptAWDSAqua+4,ScriptAWDSAqua+5,ScriptAWDSAqua+6
-	setCOvictoryText        coAWDSAqua,ScriptAWDSAqua+7
+	setCOName               coAWDSSubaru,ScriptAWDSSubaru
+	setCOpowerName          coAWDSSubaru,ScriptAWDSSubaru+1
+	setCOsuperName          coAWDSSubaru,ScriptAWDSSubaru+2
+	setCOpowerText          coAWDSSubaru,ScriptAWDSSubaru+8,ScriptAWDSAqua+9,ScriptAWDSAqua+10,ScriptAWDSAqua+11,ScriptAWDSAqua+12,ScriptAWDSAqua+13
+	setCOprofileText        coAWDSSubaru,ScriptAWDSSubaru+3,ScriptAWDSAqua+4,ScriptAWDSAqua+5,ScriptAWDSAqua+6
+	setCOvictoryText        coAWDSSubaru,ScriptAWDSSubaru+7
 
 	setCOFaction		coAWDSAqua,ArmyGreenEarth
 	setCOMusic          	coAWDSAqua,MusicAquaTheme
