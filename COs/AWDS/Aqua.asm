4bf9fdea4b8f7d7d8b119a56d0a7ec2525196c19
Zerom999 <maregee@hotmail.com>
2021-08-01 17:31:50 -0230

    Update Aqua.asm

--- COs/AWDS/Aqua.asm
+++ COs/AWDS/Aqua.asm
@@ -27,21 +27,15 @@
 	setCOD2DLandStat	coAWDSAqua,0,0,0,0
 	setCOD2DNavyStat	coAWDSAqua,10,20,1,0
 	setCOD2DAirStat		coAWDSAqua,-20,0,0,0
-	setCOD2DTransportStat   coAWDSAqua,0,0,1,0
-
+	
 	setCOPowerLandStat	coAWDSAqua,0,0,0,0
 	setCOPowerNavyStat	coAWDSAqua,20,40,2,0
 	setCOPowerAirStat	coAWDSAqua,-10,0,0,0
-	setCOPowerTransportStat coAWDSAqua,0,0,1,0
-
+	
 	setCOSuperLandStat	coAWDSAqua,0,0,0,0
 	setCOSuperAirStat	coAWDSAqua,-10,0,0,0
 	setCOSuperNavyStat	coAWDSAqua,30,60,3,0
-	setCOSuperTransportStat coAWDSAqua,0,0,1,0
-
-	setCOGFXPowerFX		coAWDSAqua,COPEffectResupplyUnit
-	setCOGFXSuperFX		coAWDSAqua,COPEffectResupplyUnit
-
+	
 	setCOAIPowerRange       coAWDSAqua,25
 	setCOAIPowerTrigger     coAWDSAqua,95
 ; blank line
