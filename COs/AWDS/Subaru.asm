34831c2d9e2563b1b3a5eb36375963ef360ab8f7
Zerom999 <maregee@hotmail.com>
2021-08-01 19:52:35 -0230

    Create Subaru.asm

---
+++ COs/AWDS/Subaru.asm
@@ -0,0 +1,41 @@
+setCODefaults           coAWDSAqua
+	setCOGameID		coAWDSAqua,Game_AWDS
+
+	setCOGFXFullbody        coAWDSAqua,GraphicsAWDSAquaGraphic
+	setCOGFXName            coAWDSAqua,GraphicsAWDSAquaName
+	setCOGFXPalette         coAWDSAqua,GraphicsAWDSAquaPalette
+	setCOGFXFace            coAWDSAqua,GraphicsAWDSAquaFace,GraphicsAWDSAquaHappyFace,GraphicsAWDSAquaSadFace
+	setCOGFXSmallFace       coAWDSAqua,GraphicsAWDSAquaMugshot
+	setCOGFXPowerAnim       coAWDSAqua,0x04,0x00
+	setCOGFXPowerSFX        coAWDSAqua,0xba,0xbb
+	setCOGFXSuperAnim       coAWDSAqua,7,7
+	setCOGFXSuperSFX        coAWDSAqua,0x1f7,0x1f8
+	
+	setCOName               coAWDSAqua,ScriptAWDSAqua
+	setCOpowerName          coAWDSAqua,ScriptAWDSAqua+1
+	setCOsuperName          coAWDSAqua,ScriptAWDSAqua+2
+	setCOpowerText          coAWDSAqua,ScriptAWDSAqua+8,ScriptAWDSAqua+9,ScriptAWDSAqua+10,ScriptAWDSAqua+11,ScriptAWDSAqua+12,ScriptAWDSAqua+13
+	setCOprofileText        coAWDSAqua,ScriptAWDSAqua+3,ScriptAWDSAqua+4,ScriptAWDSAqua+5,ScriptAWDSAqua+6
+	setCOvictoryText        coAWDSAqua,ScriptAWDSAqua+7
+
+	setCOFaction		coAWDSAqua,ArmyGreenEarth
+	setCOMusic          	coAWDSAqua,MusicAquaTheme
+
+	setCOPowerCost          coAWDSAqua,3
+	setCOSuperCost          coAWDSAqua,6
+	
+	setCOD2DLandStat	coAWDSAqua,0,0,0,0
+	setCOD2DNavyStat	coAWDSAqua,0,20,1,0
+	setCOD2DAirStat		coAWDSAqua,-20,0,0,0
+	
+	setCOPowerLandStat	coAWDSAqua,0,0,0,0
+	setCOPowerNavyStat	coAWDSAqua,0,40,2,0
+	setCOPowerAirStat	coAWDSAqua,-10,0,0,0
+	
+	setCOSuperLandStat	coAWDSAqua,0,0,0,0
+	setCOSuperNavyStat	coAWDSAqua,0,60,3,0
+	setCOSuperAirStat	coAWDSAqua,-10,0,0,0
+	
+	setCOAIPowerRange       coAWDSAqua,25
+	setCOAIPowerTrigger     coAWDSAqua,95
+; blank line
