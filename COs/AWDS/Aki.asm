5b5e5666bc10deed04b6be57ed010b004b47d7b4
Zerom999 <maregee@hotmail.com>
2021-08-01 22:11:50 -0230

    Update Aki.asm

--- COs/AWDS/Aki.asm
+++ COs/AWDS/Aki.asm
@@ -32,6 +32,9 @@
 	setCOPowerAllStat	coAWDSAki,-20,20,0,0		
 	setCOPowerCounter	coAWDSAki,50	
 	
+	setCOPowerAllStat	coAWDSAki,-20,20,0,0		
+	setCOPowerCounter	coAWDSAki,50	
+	
 	setCOAIPowerRange       coAWDSSonja,15
 	setCOAIPowerTrigger     coAWDSSonja,70
 ; blank line
