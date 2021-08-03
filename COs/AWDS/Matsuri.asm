9dcc1ca9671560fe4b22b64a63f41d9a6450acb3
Zerom999 <maregee@hotmail.com>
2021-08-02 21:42:43 -0230

    Update Matsuri.asm

--- COs/AWDS/Matsuri.asm
+++ COs/AWDS/Matsuri.asm
@@ -1,22 +1,22 @@
         setCODefaults           coAWDSMatsuri
 	setCOGameID		coAWDSMatsuri,Game_AWDS
 
-	setCOGFXFullbody        coAWDSMatsuri,GraphicsAWDSJessGraphic
-	setCOGFXName            coAWDSMatsuri,GraphicsAWDSJessName
-	setCOGFXPalette         coAWDSMatsuri,GraphicsAWDSJessPalette
-	setCOGFXFace            coAWDSMatsuri,GraphicsAWDSJessFace,GraphicsAWDSJessHappyFace,GraphicsAWDSJessSadFace
-	setCOGFXSmallFace       coAWDSMatsuri,GraphicsAWDSJessMugshot
+	setCOGFXFullbody        coAWDSMatsuri,GraphicsAWDSMatsuriGraphic
+	setCOGFXName            coAWDSMatsuri,GraphicsAWDSMatsuriName
+	setCOGFXPalette         coAWDSMatsuri,GraphicsAWDSMatsuriPalette
+	setCOGFXFace            coAWDSMatsuri,GraphicsAWDSMatsuriFace,GraphicsAWDSMatsuriHappyFace,GraphicsAWDSMatsuriSadFace
+	setCOGFXSmallFace       coAWDSMatsuri,GraphicsAWDSMatsuriMugshot
 	setCOGFXPowerAnim       coAWDSMatsuri,0x04,0x00
 	setCOGFXPowerSFX        coAWDSMatsuri,0xba,0xbb
 	setCOGFXSuperAnim       coAWDSMatsuri,7,7
 	setCOGFXSuperSFX        coAWDSMatsuri,0x1f7,0x1f8
 	
-	setCOName               coAWDSJess,ScriptAWDSJess
-	setCOpowerName          coAWDSJess,ScriptAWDSJess+1
-	setCOsuperName          coAWDSJess,ScriptAWDSJess+2
-	setCOpowerText          coAWDSJess,ScriptAWDSJess+8,ScriptAWDSJess+9,ScriptAWDSJess+10,ScriptAWDSJess+11,ScriptAWDSJess+12,ScriptAWDSJess+13
-	setCOprofileText        coAWDSJess,ScriptAWDSJess+3,ScriptAWDSJess+4,ScriptAWDSJess+5,ScriptAWDSJess+6
-	setCOvictoryText        coAWDSJess,ScriptAWDSJess+7
+	setCOName               coAWDSMatsuri,ScriptAWDSMatsuri
+	setCOpowerName          coAWDSMatsuri,ScriptAWDSMatsuri+1
+	setCOsuperName          coAWDSMatsuri,ScriptAWDSMatsuri+2
+	setCOpowerText          coAWDSMatsuri,ScriptAWDSMatsuri+8,ScriptAWDSJess+9,ScriptAWDSJess+10,ScriptAWDSJess+11,ScriptAWDSJess+12,ScriptAWDSJess+13
+	setCOprofileText        coAWDSMatsuri,ScriptAWDSMatsuri+3,ScriptAWDSJess+4,ScriptAWDSJess+5,ScriptAWDSJess+6
+	setCOvictoryText        coAWDSMatsuri,ScriptAWDSMatsuri+7
 
 	setCOFaction		coAWDSJess,ArmyGreenEarth
 	setCOMusic          	coAWDSJess,MusicJessTheme
