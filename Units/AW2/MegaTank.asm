18835470e14155ce761568ba264f83dcc38006ef
Zerom999 <maregee@hotmail.com>
2021-08-04 14:26:05 -0230

    Create MegaTank.asm

---
+++ Units/AW2/MegaTank.asm
@@ -0,0 +1,64 @@
+;Unit_Neotank
+	setUnitDefaults		Unit_NeoTank
+	
+	setUnitName		Unit_NeoTank,2236
+	setUnitPrimaryName	Unit_NeoTank,2267
+	setUnitSecondaryName	Unit_NeoTank,2253
+	
+	setUnitPrice		Unit_NeoTank,2000
+	setUnitMove		Unit_NeoTank,6
+	setUnitAmmo		Unit_NeoTank,9
+	setUnitFuel		Unit_NeoTank,99
+	setUnitVision		Unit_NeoTank,1
+
+	setUnitClass		Unit_NeoTank,ClassVehicle
+	setUnitMoveType		Unit_NeoTank,MoveTread
+	setUnitBuildLocation	Unit_NeoTank,BuildBase
+	setUnitSupplyTerrain	Unit_NeoTank,SuppliedByBase
+	setUnitFuelBurn		Unit_NeoTank,FuelUseageTypeGround
+	
+
+	setUnitAIType			Unit_NeoTank,AIDirectCombat
+	setUnitAIPriorityTarget		Unit_NeoTank,AITargetGround
+	setUnitAIUnitClass		Unit_NeoTank,AITargetGround
+	
+	setUnitDamagePrimary		Unit_NeoTank,35,Unit_Infantry
+	setUnitDamagePrimary		Unit_NeoTank,35,Unit_Mech
+	
+	setUnitDamagePrimary		Unit_NeoTank,115,Unit_Tank
+	setUnitDamagePrimary		Unit_NeoTank,85,Unit_MidTank
+	setUnitDamagePrimary		Unit_NeoTank,55,Unit_NeoTank
+	
+	setUnitDamagePrimary		Unit_NeoTank,135,Unit_Artillery
+	setUnitDamagePrimary		Unit_NeoTank,135,Unit_Rockets
+	setUnitDamagePrimary		Unit_NeoTank,135,Unit_Missiles
+
+	setUnitDamagePrimary		Unit_NeoTank,125,Unit_Recon
+	setUnitDamagePrimary		Unit_NeoTank,125,Unit_APC
+	setUnitDamagePrimary		Unit_NeoTank,115,Unit_AntiAir
+
+	setUnitDamagePrimary		Unit_NeoTank,35,Unit_Battleship
+	setUnitDamagePrimary		Unit_NeoTank,55,Unit_Cruiser
+	setUnitDamagePrimary		Unit_NeoTank,55,Unit_Lander
+	setUnitDamagePrimary		Unit_NeoTank,55,Unit_Sub
+
+	setUnitDamageSecondary		Unit_NeoTank,125,Unit_Infantry
+	setUnitDamageSecondary		Unit_NeoTank,115,Unit_Mech
+	
+	setUnitDamageSecondary		Unit_NeoTank,15,Unit_Tank
+	setUnitDamageSecondary		Unit_NeoTank,05,Unit_MidTank
+	setUnitDamageSecondary		Unit_NeoTank,01,Unit_NeoTank
+	
+	setUnitDamageSecondary		Unit_NeoTank,75,Unit_Artillery
+	setUnitDamageSecondary		Unit_NeoTank,75,Unit_Rockets
+	setUnitDamageSecondary		Unit_Neotank,75,Unit_Missiles
+
+	setUnitDamageSecondary		Unit_NeoTank,75,Unit_Recon
+	setUnitDamageSecondary		Unit_NeoTank,65,Unit_APC
+	setUnitDamageSecondary		Unit_NeoTank,35,Unit_AntiAir
+
+	setUnitDamageSecondary		Unit_NeoTank,55,Unit_Battlecopter
+	setUnitDamageSecondary		Unit_NeoTank,95,Unit_TransportCopter
+
+	setUnitInventionDamage		Unit_NeoTank,75,01
+	
