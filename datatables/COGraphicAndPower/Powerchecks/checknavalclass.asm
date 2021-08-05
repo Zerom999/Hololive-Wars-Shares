78fe5e06753ceaa5e396f6b8c2c49e5c5ca60091
Zerom999 <maregee@hotmail.com>
2021-08-05 18:10:06 -0230

    Update and rename Checknavalclass.asm to checknavalclass.asm

--- datatables/COGraphicAndPower/Powerchecks/checknavalclass.asm
+++ datatables/COGraphicAndPower/Powerchecks/checknavalclass.asm
@@ -1,13 +1,13 @@
-;checkaerialclass.asm
+;checknavalclass.asm
 
 .org 0x08044488
-COPCheckAerialClass:
+COPCheckNavalClass:
 	mov	r3,r0
 	ldrb	r1,[r3,0x1]
 	mov	r0,0x8
 	and	r0,r1
 	cmp	r0,0x0
-	bne	COPCheckAerialClassFalse
+	bne	COPCheckNavalClassFalse
 	ldr	r2,=0x085D5ABC
 	ldrb	r1,[r3]
 	mov	r0,0x5C
@@ -15,15 +15,15 @@ COPCheckAerialClass:
 	add	r0,r0,r2
 	ldrb	r0,[r0,0x18]
 	cmp	r0,0x2
-	beq	COPCheckAerialClassTrue
+	beq	COPCheckNavalClassTrue
 	cmp	r0,0x3
-	beq	COPCheckAerialClassTrue
+	beq	COPCheckNavalClassTrue
 COPCheckAerialClassFalse:
 	mov	r0,0x0
-	b	COPCheckAerialClassEnd
+	b	COPCheckNavalClassEnd
 	.pool
-COPCheckAerialClassTrue:
+COPCheckNavalClassTrue:
 	mov	r0,0x1
-COPCheckAerialClassEnd:
+COPCheckNavalClassEnd:
 	bx	r14
 ;Blank Line
