25a75f00393e8a7cdc89be5aaa51e00537c0c535
Zerom999 <maregee@hotmail.com>
2021-08-05 00:37:16 -0230

    Create Polka.asm

---
+++ COs/AWDS/Polka.asm
@@ -0,0 +1,35 @@
+setCODefaults           coAWDSNoel
+	setCOGameID		coAWDSNoel,Game_AWDS
+
+	setCOGFXFullbody        coAWDSNoel,GraphicsAWDSNoelGraphic
+	setCOGFXName            coAWDSNoel,GraphicsAWDSNoelName
+	setCOGFXPalette         coAWDSNoel,GraphicsAWDSNoelPalette
+	setCOGFXFace            coAWDSNoel,GraphicsAWDSNoelFace,GraphicsAWDSNoelHappyFace,GraphicsAWDSNoelSadFace
+	setCOGFXSmallFace       coAWDSNoel,GraphicsAWDSNoelMugshot
+	setCOGFXPowerAnim       coAWDSNoel,0,0
+	setCOGFXPowerSFX        coAWDSNoel,0xab,0xac
+	setCOGFXSuperAnim       coAWDSNoel,7,7
+	setCOGFXSuperSFX        coAWDSNoel,0x1f7,0x1f8
+	
+	setCOName               coAWDSNoel,ScriptAWDSNoel
+	setCOpowerName          coAWDSNoel,ScriptAWDSNoel+1
+	setCOsuperName          coAWDSNoel,ScriptAWDSNoel+2
+	setCOpowerText          coAWDSNoel,ScriptAWDSNoel+8,ScriptAWDSNoel+9,ScriptAWDSNoel+10,ScriptAWDSNoel+11,ScriptAWDSNoel+12,ScriptAWDSNoel+13
+	setCOprofileText        coAWDSNoel,ScriptAWDSNoel+3,ScriptAWDSNoel+4,ScriptAWDSNoel+5,ScriptAWDSNoel+6
+	setCOvictoryText        coAWDSNoel,ScriptAWDSNoel+7
+
+	setCOFaction		coAWDSNoel,ArmyYellowComet
+	setCOMusic          	coAWDSNoel,MusicNoelTheme
+
+	setCOPowerCost          coAWDSNoel,4
+	setCOSuperCost          coAWDSNoel,8
+
+	setCOD2DAllStat		coAWDSNoel,20,20,-1,0
+		
+	setCOPowerAllStat	coAWDSNoel,40,40,-1,0
+	
+	setCOSuperAllStat	coAWDSNoel,60,60,-1,0
+	
+	setCOAIPowerRange       coAWDSNoel,25
+	setCOAIPowerTrigger     coAWDSNoel,95
+; blank line
