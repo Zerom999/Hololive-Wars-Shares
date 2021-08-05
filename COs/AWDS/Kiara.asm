5621475ee7f828a6217657b11ad200d860e0cc0f
Zerom999 <maregee@hotmail.com>
2021-08-05 17:58:07 -0230

    Create Kiara.asm

---
+++ COs/AWDS/Kiara.asm
@@ -0,0 +1,41 @@
+setCODefaults           coAWDSGura
+	setCOGameID		coAWDSGura,Game_AWDS
+
+	setCOGFXFullbody        coAWDSGura,GraphicsAWDSGuraGraphic
+	setCOGFXName            coAWDSGura,GraphicsAWDSGuraName
+	setCOGFXPalette         coAWDSGura,GraphicsAWDSGuraPalette
+	setCOGFXFace            coAWDSGura,GraphicsAWDSGuraFace,GraphicsAWDSGuraHappyFace,GraphicsAWDSGuraSadFace
+	setCOGFXSmallFace       coAWDSGura,GraphicsAWDSGuraMugshot
+	setCOGFXPowerAnim       coAWDSGura,0x04,0x00
+	setCOGFXPowerSFX        coAWDSGura,0xba,0xbb
+	setCOGFXSuperAnim       coAWDSGura,7,7
+	setCOGFXSuperSFX        coAWDSGura,0x1f7,0x1f8
+	
+	setCOName               coAWDSGura,ScriptAWDSGura
+	setCOpowerName          coAWDSGura,ScriptAWDSGura+1
+	setCOsuperName          coAWDSGura,ScriptAWDSGura+2
+	setCOpowerText          coAWDSGura,ScriptAWDSGura+8,ScriptAWDSGura+9,ScriptAWDSGura+10,ScriptAWDSGura+11,ScriptAWDSGura+12,ScriptAWDSGura+13
+	setCOprofileText        coAWDSGura,ScriptAWDSGura+3,ScriptAWDSGura+4,ScriptAWDSGura+5,ScriptAWDSGura+6
+	setCOvictoryText        coAWDSGura,ScriptAWDSGura+7
+
+	setCOFaction		coAWDSGura,ArmyGreenEarth
+	setCOMusic          	coAWDSGura,MusicGuraTheme
+
+	setCOPowerCost          coAWDSGura,4
+	setCOSuperCost          coAWDSGura,8
+	
+	setCOD2DLandStat	coAWDSGura,0,0,0,0
+	setCOD2DNavyStat	coAWDSGura,60,0,1,0
+	setCOD2DAirStat		coAWDSGura,-50,0,-1,0
+	
+	setCOPowerLandStat	coAWDSGura,0,0,0,0
+	setCOPowerNavyStat	coAWDSGura,90,0,2,0
+	setCOPowerAirStat	coAWDSGura,-40,0,-1,0
+	
+	setCOSuperLandStat	coAWDSGura,0,0,0,0
+	setCOSuperNavyStat	coAWDSGura,120,0,3,0
+	setCOSuperAirStat	coAWDSGura,-40,0,-1,0
+	
+	setCOAIPowerRange       coAWDSGura,25
+	setCOAIPowerTrigger     coAWDSGura,95
+; blank line
