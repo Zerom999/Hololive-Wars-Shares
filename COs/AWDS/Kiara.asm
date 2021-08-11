29e140b802cddb40e2eb7f1454594fc267d047d1
Zerom999 <maregee@hotmail.com>
2021-08-11 17:28:00 -0230

    Update Kiara.asm

--- COs/AWDS/Kiara.asm
+++ COs/AWDS/Kiara.asm
@@ -36,6 +36,9 @@
 	setCOSuperNavyStat	coAWDSKiara,-40,0,-1,0
 	setCOSuperAirStat	coAWDSKiara,120,0,3,0
 	
+	setCOGFXPowerRule	coAWDSEagle,COPCheckAerialClass
+	setCOGFXSuperRule	coAWDSEagle,COPCheckAerialClass
+	
 	setCOAIPowerRange       coAWDSKiara,25
 	setCOAIPowerTrigger     coAWDSKiara,95
 ; blank line
