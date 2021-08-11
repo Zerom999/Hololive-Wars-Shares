90ecad4edd5b4d4b8e6d87ae57f8658fe03fc31f
Zerom999 <maregee@hotmail.com>
2021-08-11 17:33:25 -0230

    Update Calliope.asm

--- COs/AWDS/Calliope.asm
+++ COs/AWDS/Calliope.asm
@@ -30,8 +30,8 @@
 	setCOSuperDirectStat	coAWDSCalliope,90,0,1,0	
 	setCOSuperIndirectStat	coAWDSCalliope,-40,0,0,-1	
 	
-	setCOD2DDirectStat	coAWDSCalliope,120,0,2,0	
-	setCOD2DIndirectStat	coAWDSCalliope,-40,0,0,-1	
+	setCOGFXPowerRule       coAWDSCalliope,COPCheckDirectCombatUnits
+	setCOGFXSuperRule       coAWDSCalliope,COPCheckDirectCombatUnits
 	
 	setCOAIPowerRange       coAWDSCalliope,25
 	setCOAIPowerTrigger     coAWDSCalliope,95
