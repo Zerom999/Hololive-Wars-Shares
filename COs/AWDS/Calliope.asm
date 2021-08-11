e248c9a268c10b62d9d2842499aa378cc074fa74
Zerom999 <maregee@hotmail.com>
2021-08-11 15:56:25 -0230

    Create Calliope.asm

---
+++ COs/AWDS/Calliope.asm
@@ -0,0 +1,41 @@
+setCODefaults           coAWDSKiara
+	setCOGameID		coAWDSKiara,Game_AWDS
+
+	setCOGFXFullbody        coAWDSKiara,GraphicsAWDSKiaraGraphic
+	setCOGFXName            coAWDSKiara,GraphicsAWDSKiaraName
+	setCOGFXPalette         coAWDSKiara,GraphicsAWDSKiaraPalette
+	setCOGFXFace            coAWDSKiara,GraphicsAWDSKiaraFace,GraphicsAWDSKiaraHappyFace,GraphicsAWDSKiaraSadFace
+	setCOGFXSmallFace       coAWDSKiara,GraphicsAWDSKiaraMugshot
+	setCOGFXPowerAnim       coAWDSKiara,0x04,0x00
+	setCOGFXPowerSFX        coAWDSKiara,0xba,0xbb
+	setCOGFXSuperAnim       coAWDSKiara,7,7
+	setCOGFXSuperSFX        coAWDSKiara,0x1f7,0x1f8
+	
+	setCOName               coAWDSKiara,ScriptAWDSKiara
+	setCOpowerName          coAWDSKiara,ScriptAWDSKiara+1
+	setCOsuperName          coAWDSKiara,ScriptAWDSKiara+2
+	setCOpowerText          coAWDSKiara,ScriptAWDSKiara+8,ScriptAWDSKiara+9,ScriptAWDSKiara+10,ScriptAWDSKiara+11,ScriptAWDSKiara+12,ScriptAWDSKiara+13
+	setCOprofileText        coAWDSKiara,ScriptAWDSKiara+3,ScriptAWDSKiara+4,ScriptAWDSKiara+5,ScriptAWDSKiara+6
+	setCOvictoryText        coAWDSKiara,ScriptAWDSKiara+7
+
+	setCOFaction		coAWDSKiara,ArmyGreenEarth
+	setCOMusic          	coAWDSKiara,MusicKiaraTheme
+
+	setCOPowerCost          coAWDSKiara,4
+	setCOSuperCost          coAWDSKiara,8
+	
+	setCOD2DLandStat	coAWDSKiara,0,0,0,0
+	setCOD2DNavyStat	coAWDSKiara,-50,0,-1,0
+	setCOD2DAirStat		coAWDSKiara,60,0,1,0
+	
+	setCOPowerLandStat	coAWDSKiara,0,0,0,0
+	setCOPowerNavyStat	coAWDSKiara,-40,0,-1,0
+	setCOPowerAirStat	coAWDSKiara,90,0,2,0
+	
+	setCOSuperLandStat	coAWDSKiara,0,0,0,0
+	setCOSuperNavyStat	coAWDSKiara,-40,0,-1,0
+	setCOSuperAirStat	coAWDSKiara,120,0,3,0
+	
+	setCOAIPowerRange       coAWDSKiara,25
+	setCOAIPowerTrigger     coAWDSKiara,95
+; blank line
