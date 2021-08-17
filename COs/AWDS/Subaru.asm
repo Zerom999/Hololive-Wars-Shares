85f8c85848573d92a0f8527f3306bccd3a59808e
Zerom999 <maregee@hotmail.com>
2021-08-17 20:13:47 -0230

    Update Subaru.asm

--- COs/AWDS/Subaru.asm
+++ COs/AWDS/Subaru.asm
@@ -26,7 +26,9 @@
 
 	setCOD2DDirectStat	coAWDSSubaru,30,0,0,0	
 	setCOD2DIndirectStat	coAWDSSubaru,-20,0,0,0	
-	setCOD2DUnitStat	coAWDSSubaru,Unit_BattleCopter,50,0,0,0	
+	setCOD2DUnitStat	coAWDSSubaru,Unit_Recon,50,0,0,0
+	setCOD2DUnitStat	coAWDSSubaru,Unit_Rockets,50,0,0,0
+	setCOD2DUnitStat	coAWDSSubaru,Unit_Missile,50,0,0,0
 	
 	setCOPowerDirectStat	coAWDSSubaru,30,0,0,0	
 	setCOPowerIndirectStat	coAWDSSubaru,-20,0,0,0
