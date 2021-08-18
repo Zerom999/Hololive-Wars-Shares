482e035e00d3d414ceaf8c87fa02fcc91f89eec1
Zerom999 <maregee@hotmail.com>
2021-08-18 15:52:45 -0230

    Update Sora.asm

--- COs/AWDS/Sora.asm
+++ COs/AWDS/Sora.asm
@@ -24,11 +24,11 @@
 	setCOPowerCost          coAWDSSora,5
 	setCOSuperCost          coAWDSSora,10
 
-	setCOPowerDirectStat	coAWDSSora,30,30,1,1	
-	setCOPowerIndirectStat	coAWDSSora,30,30,1,1
+	setCOPowerDirectStat	coAWDSSora,30,20,1,1	
+	setCOPowerIndirectStat	coAWDSSora,30,20,1,1
 	
-	setCOSuperDirectStat	coAWDSSora,60,0,0,0	
-	setCOSuperIndirectStat	coAWDSSora,60,0,0,0
+	setCOSuperDirectStat	coAWDSSora,60,40,2,2	
+	setCOSuperIndirectStat	coAWDSSora,60,40,2,2
 	
 	setCOAIPowerRange       coAWDSSora,25
 	setCOAIPowerTrigger     coAWDSSora,95
