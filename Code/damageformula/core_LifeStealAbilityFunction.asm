927b199112ffa39f9218d62a2b05abbe41e3fb35
Zerom999 <maregee@hotmail.com>
2021-08-19 17:31:54 -0230

    Update core_LifeStealAbilityFunction.asm

--- Code/damageformula/core_LifeStealAbilityFunction.asm
+++ Code/damageformula/core_LifeStealAbilityFunction.asm
@@ -59,3 +59,36 @@ DamageReflectionAbilityFunction:
 	ldrb	r1,[r1,0x1E]
 	mov	r2,Gather_CO_DamageReflection
 	bl	GatherCOAbility
+	
+	cmp	r0,0
+	beq	@DamageReflectionSubroutineEnd
+	mul	r0,r3
+	mov	r1,100
+	bl	DivisionFunction
+	
+	ldr	r1,[r5]
+	ldrb	r1,[r1,0x4]
+	mov	r2,0x7F
+	and	r1,r2
+	cmp	r1,0
+	ble	@DamageReflectionSubroutineEnd
+	
+	sub	r0,r1,r0
+	cmp	r0,0
+	bgt	@DamageReflectionSubroutineSaveDamage
+	mov	r0,1
+@DamageReflectionSubroutineSaveDamage:	
+	ldr	r1,[r5]
+	ldrb	r1,[r1,0x4]
+	mov	r2,0x80
+	and	r1,r2
+	orr	r0,r1
+	ldr	r1,[r5]
+	strb	r0,[r1,0x4]
+
+@DamageReflectionSubroutineEnd:	
+	pop	{r4,r5}
+	pop	{r0}
+	bx	r0
+	.pool
+	
