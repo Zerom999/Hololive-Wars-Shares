831be74aa205e4ec6bb7de24d5d9034de46caecf
Zerom999 <maregee@hotmail.com>
2021-08-23 16:53:30 -0230

    Update Moona.asm

--- COs/AWDS/Moona.asm
+++ COs/AWDS/Moona.asm
@@ -1,1 +1,42 @@
+setCODefaults           coAWDSFubuki
+	setCOGameID		coAWDSFubuki,Game_AWDS
+
+	setCOGFXFullbody        coAWDSFubuki,GraphicsAWDSFubukiGraphic
+	setCOGFXName            coAWDSFubuki,GraphicsAWDSFubukiName
+	setCOGFXPalette         coAWDSFubuki,GraphicsAWDSFubukiPalette
+	setCOGFXFace            coAWDSFubuki,GraphicsAWDSFubukiFace,GraphicsAWDSFubukiHappyFace,GraphicsAWDSFubukiSadFace
+	setCOGFXSmallFace       coAWDSFubuki,GraphicsAWDSFubukiMugshot
+	setCOGFXPowerAnim       coAWDSFubuki,0,0
+	setCOGFXPowerSFX        coAWDSFubuki,0xC3,0
+	setCOGFXSuperAnim       coAWDSFubuki,7,7
+	setCOGFXSuperSFX        coAWDSFubuki,0x1f7,0x1f8
+	
+	setCOName               coAWDSFubuki,ScriptAWDSFubuki
+	setCOpowerName          coAWDSFubuki,ScriptAWDSFubuki+1
+	setCOsuperName          coAWDSFubuki,ScriptAWDSFubuki+2
+	setCOpowerText          coAWDSFubuki,ScriptAWDSFubuki+8,ScriptAWDSFubuki+9,ScriptAWDSFubuki+10,ScriptAWDSFubuki+11,ScriptAWDSFubuki+12,ScriptAWDSFubuki+13
+	setCOprofileText        coAWDSFubuki,ScriptAWDSFubuki+3,ScriptAWDSFubuki+4,ScriptAWDSFubuki+5,ScriptAWDSFubuki+6
+	setCOvictoryText        coAWDSFubuki,ScriptAWDSFubuki+7
+
+	setCOFaction		coAWDSFubuki,ArmyBlueMoon
+	setCOMusic          	coAWDSFubuki,MusicFubukiTheme
+
+	setCOPowerCost          coAWDSFubuki,4
+	setCOSuperCost          coAWDSFubuki,8
+
+	setCOD2DSnowFirepower	coAWDSFubuki,20,0,1,0
+	setCOPowerSnowFirepower	coAWDSFubuki,20,0,1,0
+	setCOSuperSnowFirepower	coAWDSFubuki,20,0,1,0
+	
+	setCOD2DMove		coAWDSFubuki, MovementChartClear,MovementChartClear,MovementChartSnow
+	setCOPowerMove		coAWDSFubuki, MovementChartClear,MovementChartClear,MovementChartSnow
+	setCOSuperMove		coAWDSFubuki, MovementChartClear,MovementChartClear,MovementChartSnow
+	
+	setCOPowerASM		coAWDSFubuki,COPowerExtendedBlizzard
+	setCOSuperASM		coAWDSFubuki,COPowerExtendedWinterFury
+	
+	setCOAIPowerRange       coAWDSFubuki,15
+	setCOAIPowerTrigger     coAWDSFubuki,70
+	setCOAIPowerTriggerASM	coAWDSOlaf,AIActivateNotSnow
+; blank line
 
