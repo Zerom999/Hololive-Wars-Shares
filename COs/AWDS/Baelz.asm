6650c6661480cfa418588459bb1d3cf337bffa87
Zerom999 <maregee@hotmail.com>
2021-08-23 21:27:21 -0230

    Update Baelz.asm

--- COs/AWDS/Baelz.asm
+++ COs/AWDS/Baelz.asm
@@ -1,1 +1,33 @@
+setCODefaults           coAWDSOkayu
+	setCOGameID		coAWDSOkayu,Game_AWDS
+
+	setCOGFXFullbody        coAWDSOkayu,GraphicsAWDSOkayuGraphic
+	setCOGFXName            coAWDSOkayu,GraphicsAWDSOkayuName
+	setCOGFXPalette         coAWDSOkayu,GraphicsAWDSOkayuPalette
+	setCOGFXFace            coAWDSOkayu,GraphicsAWDSOkayuFace,GraphicsAWDSOkayuHappyFace,GraphicsAWDSOkayuSadFace
+	setCOGFXSmallFace       coAWDSOkayu,GraphicsAWDSOkayuMugshot
+	setCOGFXPowerAnim       coAWDSOkayu,0,0
+	setCOGFXPowerSFX        coAWDSOkayu,0xB4,0xB4
+	setCOGFXSuperAnim       coAWDSOkayu,7,7
+	setCOGFXSuperSFX        coAWDSOkayu,0x1F4,0x1f5
+	
+	setCOName               coAWDSOkayu,ScriptAWDSOkayu
+	setCOpowerName          coAWDSOkayu,ScriptAWDSOkayu+1
+	setCOsuperName          coAWDSOkayu,ScriptAWDSOkayu+2
+	setCOpowerText          coAWDSOkayu,ScriptAWDSOkayu+8,ScriptAWDSOkayu+9,ScriptAWDSOkayu+10,ScriptAWDSOkayu+11,ScriptAWDSOkayu+12,ScriptAWDSOkayu+13
+	setCOprofileText        coAWDSOkayu,ScriptAWDSOkayu+3,ScriptAWDSOkayu+4,ScriptAWDSOkayu+5,ScriptAWDSOkayu+6
+	setCOvictoryText        coAWDSOkayu,ScriptAWDSOkayu+7
+
+	setCOFaction		coAWDSOkayu,ArmyGreenEarth
+	setCOMusic          	coAWDSOkayu,MusicOkayuTheme
+	
+	setCOPowerCost          coAWDSOkayu,2
+	setCOSuperCost          coAWDSOkayu,4
+	
+	setCOPowerAllStat	coAWDSOkayu,10,0,1,0
+	setCOSuperAllStat	coAWDSOkayu,20,0,2,0
+
+	setCOAIPowerRange       coAWDSOkayu,35
+	setCOAIPowerTrigger     coAWDSOkayu,95
+; blank line
 
