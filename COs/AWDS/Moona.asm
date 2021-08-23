8a94921cb331a7a542bf8e617f5994e6fc23dcdd
Zerom999 <maregee@hotmail.com>
2021-08-23 17:14:48 -0230

    Update Moona.asm

--- COs/AWDS/Moona.asm
+++ COs/AWDS/Moona.asm
@@ -1,28 +1,28 @@
-        setCODefaults           coAWDSFubuki
-	setCOGameID		coAWDSFubuki,Game_AWDS
+        setCODefaults           coAWDSMoona
+	setCOGameID		coAWDSMoona,Game_AWDS
 
-	setCOGFXFullbody        coAWDSFubuki,GraphicsAWDSFubukiGraphic
-	setCOGFXName            coAWDSFubuki,GraphicsAWDSFubukiName
-	setCOGFXPalette         coAWDSFubuki,GraphicsAWDSFubukiPalette
-	setCOGFXFace            coAWDSFubuki,GraphicsAWDSFubukiFace,GraphicsAWDSFubukiHappyFace,GraphicsAWDSFubukiSadFace
-	setCOGFXSmallFace       coAWDSFubuki,GraphicsAWDSFubukiMugshot
-	setCOGFXPowerAnim       coAWDSFubuki,0,0
-	setCOGFXPowerSFX        coAWDSFubuki,0xC3,0
-	setCOGFXSuperAnim       coAWDSFubuki,7,7
-	setCOGFXSuperSFX        coAWDSFubuki,0x1f7,0x1f8
+	setCOGFXFullbody        coAWDSMoona,GraphicsAWDSFubukiGraphic
+	setCOGFXName            coAWDSMoona,GraphicsAWDSFubukiName
+	setCOGFXPalette         coAWDSMoona,GraphicsAWDSFubukiPalette
+	setCOGFXFace            coAWDSMoona,GraphicsAWDSFubukiFace,GraphicsAWDSFubukiHappyFace,GraphicsAWDSFubukiSadFace
+	setCOGFXSmallFace       coAWDSMoona,GraphicsAWDSFubukiMugshot
+	setCOGFXPowerAnim       coAWDSMoona,0,0
+	setCOGFXPowerSFX        coAWDSMoona,0xC3,0
+	setCOGFXSuperAnim       coAWDSMoona,7,7
+	setCOGFXSuperSFX        coAWDSMoona,0x1f7,0x1f8
 	
-	setCOName               coAWDSFubuki,ScriptAWDSFubuki
-	setCOpowerName          coAWDSFubuki,ScriptAWDSFubuki+1
-	setCOsuperName          coAWDSFubuki,ScriptAWDSFubuki+2
-	setCOpowerText          coAWDSFubuki,ScriptAWDSFubuki+8,ScriptAWDSFubuki+9,ScriptAWDSFubuki+10,ScriptAWDSFubuki+11,ScriptAWDSFubuki+12,ScriptAWDSFubuki+13
-	setCOprofileText        coAWDSFubuki,ScriptAWDSFubuki+3,ScriptAWDSFubuki+4,ScriptAWDSFubuki+5,ScriptAWDSFubuki+6
-	setCOvictoryText        coAWDSFubuki,ScriptAWDSFubuki+7
+	setCOName               coAWDSMoona,ScriptAWDSFubuki
+	setCOpowerName          coAWDSMoona,ScriptAWDSFubuki+1
+	setCOsuperName          coAWDSMoona,ScriptAWDSFubuki+2
+	setCOpowerText          coAWDSMoona,ScriptAWDSFubuki+8,ScriptAWDSFubuki+9,ScriptAWDSFubuki+10,ScriptAWDSFubuki+11,ScriptAWDSFubuki+12,ScriptAWDSFubuki+13
+	setCOprofileText        coAWDSMoona,ScriptAWDSFubuki+3,ScriptAWDSFubuki+4,ScriptAWDSFubuki+5,ScriptAWDSFubuki+6
+	setCOvictoryText        coAWDSMoona,ScriptAWDSFubuki+7
 
-	setCOFaction		coAWDSFubuki,ArmyBlueMoon
-	setCOMusic          	coAWDSFubuki,MusicFubukiTheme
+	setCOFaction		coAWDSMoona,ArmyBlueMoon
+	setCOMusic          	coAWDSMoona,MusicFubukiTheme
 
-	setCOPowerCost          coAWDSFubuki,4
-	setCOSuperCost          coAWDSFubuki,8
+	setCOPowerCost          coAWDSMoona,4
+	setCOSuperCost          coAWDSMoona,8
 
 	setCOD2DSnowFirepower	coAWDSFubuki,20,0,1,0
 	setCOPowerSnowFirepower	coAWDSFubuki,20,0,1,0
