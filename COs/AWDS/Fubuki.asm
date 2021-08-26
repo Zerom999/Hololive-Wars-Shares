4ed5c8b2cca6c39de8eb674f80a693324f562383
Zerom999 <maregee@hotmail.com>
2021-08-26 20:28:46 -0230

    Update Fubuki.asm

--- COs/AWDS/Fubuki.asm
+++ COs/AWDS/Fubuki.asm
@@ -25,8 +25,8 @@
 	setCOSuperCost          coAWDSFubuki,8
 	
 	setCOD2DSnowFirepower	coAWDSFubuki,20,0,1,0
-	setCOPowerSnowFirepower	coAWDSFubuki,20,0,1,0
-	setCOSuperSnowFirepower	coAWDSFubuki,20,0,1,0
+	setCOPowerSnowFirepower	coAWDSFubuki,40,0,1,0
+	setCOSuperSnowFirepower	coAWDSFubuki,60,0,1,0
 	
 	setCOD2DMove		coAWDSFubuki, MovementChartClear,MovementChartClear,MovementChartSnow
 	setCOPowerMove		coAWDSFubuki, MovementChartClear,MovementChartClear,MovementChartSnow
