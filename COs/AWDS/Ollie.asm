2abffcfaa4e70a782695517a1f93af1a62401991
Zerom999 <maregee@hotmail.com>
2021-08-30 17:33:36 -0230

    Update Ollie.asm

--- COs/AWDS/Ollie.asm
+++ COs/AWDS/Ollie.asm
@@ -1,1 +1,36 @@
+setCODefaults           coAWDSNene
+	setCOGameID		coAWDSNene,Game_AWDS
+
+	setCOGFXFullbody        coAWDSNene,GraphicsAWDSNeneGraphic
+	setCOGFXName            coAWDSNene,GraphicsAWDSNeneName
+	setCOGFXPalette         coAWDSNene,GraphicsAWDSNenePalette
+	setCOGFXFace            coAWDSNene,GraphicsAWDSNeneFace,GraphicsAWDSNeneHappyFace,GraphicsAWDSNeneSadFace
+	setCOGFXSmallFace       coAWDSNene,GraphicsAWDSNeneMugshot
+	setCOGFXPowerAnim       coAWDSNene,2,0
+	setCOGFXPowerSFX        coAWDSNene,0xb7,0xb8
+	setCOGFXSuperAnim       coAWDSNene,7,7
+	setCOGFXSuperSFX        coAWDSNene,0x1c6,0
+	
+	setCOName               coAWDSNene,ScriptAWDSNene
+	setCOpowerName          coAWDSNene,ScriptAWDSNene+1
+	setCOsuperName          coAWDSNene,ScriptAWDSNene+2
+	setCOpowerText          coAWDSNene,ScriptAWDSNene+8,ScriptAWDSNene+9,ScriptAWDSNene+10,ScriptAWDSNene+11,ScriptAWDSNene+12,ScriptAWDSNene+13
+	setCOprofileText        coAWDSNene,ScriptAWDSNene+3,ScriptAWDSNene+4,ScriptAWDSNene+5,ScriptAWDSNene+6
+	setCOvictoryText        coAWDSNene,ScriptAWDSNene+7
+
+	setCOFaction		coAWDSNene,ArmyOrangeStar
+	setCOMusic          	coAWDSNene,MusicOrangeStar
+
+	setCOPowerCost          coAWDSNene,4
+	setCOSuperCost          coAWDSNene,8
+
+	setCOd2dRepair		coAWDSNene,1
+	setCOPowerASM		coAWDSNene,TestMissile	
+	setCOPowerRepair	coAWDSNene,1
+	setCOSuperRepair	coAWDSNene,1
+	setCOSuperASM		coAWDSNene,COPowerCoveringFire
+	
+	setCOAIPowerRange       coAWDSNene,15
+	setCOAIPowerTrigger     coAWDSNene,70
+; blank line
 
