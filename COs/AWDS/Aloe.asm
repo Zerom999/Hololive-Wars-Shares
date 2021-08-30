8347608a1050db89d3d1224a504dd10120d77e2a
Zerom999 <maregee@hotmail.com>
2021-08-30 18:39:01 -0230

    Update Aloe.asm

--- COs/AWDS/Aloe.asm
+++ COs/AWDS/Aloe.asm
@@ -1,1 +1,34 @@
+setCODefaults           coAWDSTowa
+	setCOGameID		coAWDSTowa,Game_AWDS
+
+	setCOGFXFullbody        coAWDSTowa,GraphicsAWDSTowaGraphic
+	setCOGFXName            coAWDSTowa,GraphicsAWDSTowaName
+	setCOGFXPalette         coAWDSTowa,GraphicsAWDSTowaPalette
+	setCOGFXFace            coAWDSTowa,GraphicsAWDSTowaFace,GraphicsAWDSTowaHappyFace,GraphicsAWDSTowaSadFace
+	setCOGFXSmallFace       coAWDSTowa,GraphicsAWDSTowaMugshot
+	setCOGFXPowerAnim       coAWDSTowa,0,0
+	setCOGFXPowerSFX        coAWDSTowa,0xb7,0xb8
+	setCOGFXSuperAnim       coAWDSTowa,7,7
+	setCOGFXSuperSFX        coAWDSTowa,0x1f7,0x1f8
+	
+	setCOName               coAWDSTowa,ScriptAWDSTowa
+	setCOpowerName          coAWDSTowa,ScriptAWDSTowa+1
+	setCOsuperName          coAWDSTowa,ScriptAWDSTowa+2
+	setCOpowerText          coAWDSTowa,ScriptAWDSTowa+8,ScriptAWDSTowa+9,ScriptAWDSTowa+10,ScriptAWDSTowa+11,ScriptAWDSTowa+12,ScriptAWDSTowa+13
+	setCOprofileText        coAWDSTowa,ScriptAWDSTowa+3,ScriptAWDSTowa+4,ScriptAWDSTowa+5,ScriptAWDSTowa+6
+	setCOvictoryText        coAWDSTowa,ScriptAWDSTowa+7
+
+	setCOFaction		coAWDSTowa,ArmyYellowComet
+	setCOMusic          	coAWDSTowa,MusicTowaTheme
+
+	setCOPowerCost          coAWDSTowa,3
+	setCOSuperCost          coAWDSTowa,6
+
+	setCOD2DAllStat		coAWDSTowa,40,-20,0,0
+	setCOPowerAllStat	coAWDSTowa,70,-20,0,0
+	setCOSuperAllStat	coAWDSTowa,100,-20,0,0
+	
+	setCOAIPowerRange       coAWDSTowa,25
+	setCOAIPowerTrigger     coAWDSTowa,95
+; blank line
 
