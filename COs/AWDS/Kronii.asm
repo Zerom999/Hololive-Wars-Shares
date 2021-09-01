8b2688fcb9de834ec31f171a6a5ba2b0c65ac786
Zerom999 <maregee@hotmail.com>
2021-09-01 18:07:19 -0230

    Update Kronii.asm

--- COs/AWDS/Kronii.asm
+++ COs/AWDS/Kronii.asm
@@ -1,33 +1,33 @@
-        setCODefaults           coAWDSSensei
-	setCOGameID		coAWDSSensei,Game_AWDS
+        setCODefaults           coAWDSKronii
+	setCOGameID		coAWDSKronii,Game_AWDS
 
-	setCOGFXFullbody        coAWDSSensei,GraphicsAWDSSenseiGraphic
-	setCOGFXName            coAWDSSensei,GraphicsAWDSSenseiName
-	setCOGFXPalette         coAWDSSensei,GraphicsAWDSSenseiPalette
-	setCOGFXFace            coAWDSSensei,GraphicsAWDSSenseiFace,GraphicsAWDSSenseiHappyFace,GraphicsAWDSSenseiSadFace
-	setCOGFXSmallFace       coAWDSSensei,GraphicsAWDSSenseiMugshot
-	setCOGFXPowerAnim       coAWDSSensei,3,0
-	setCOGFXPowerSFX        coAWDSSensei,0xb4,0xb5
-	setCOGFXSuperAnim       coAWDSSensei,7,7
-	setCOGFXSuperSFX        coAWDSSensei,0x1f7,0x1f8
+	setCOGFXFullbody        coAWDSKronii,GraphicsAWDSSenseiGraphic
+	setCOGFXName            coAWDSKronii,GraphicsAWDSSenseiName
+	setCOGFXPalette         coAWDSKronii,GraphicsAWDSSenseiPalette
+	setCOGFXFace            coAWDSKronii,GraphicsAWDSSenseiFace,GraphicsAWDSSenseiHappyFace,GraphicsAWDSSenseiSadFace
+	setCOGFXSmallFace       coAWDSKronii,GraphicsAWDSSenseiMugshot
+	setCOGFXPowerAnim       coAWDSKronii,3,0
+	setCOGFXPowerSFX        coAWDSKronii,0xb4,0xb5
+	setCOGFXSuperAnim       coAWDSKronii,7,7
+	setCOGFXSuperSFX        coAWDSKronii,0x1f7,0x1f8
 	
-	setCOName               coAWDSSensei,ScriptAWDSSensei
-	setCOpowerName          coAWDSSensei,ScriptAWDSSensei+1
-	setCOsuperName          coAWDSSensei,ScriptAWDSSensei+2
-	setCOpowerText          coAWDSSensei,ScriptAWDSSensei+8,ScriptAWDSSensei+9,ScriptAWDSSensei+10,ScriptAWDSSensei+11,ScriptAWDSSensei+12,ScriptAWDSSensei+13
-	setCOprofileText        coAWDSSensei,ScriptAWDSSensei+3,ScriptAWDSSensei+4,ScriptAWDSSensei+5,ScriptAWDSSensei+6
-	setCOvictoryText        coAWDSSensei,ScriptAWDSSensei+7
+	setCOName               coAWDSKronii,ScriptAWDSSensei
+	setCOpowerName          coAWDSKronii,ScriptAWDSSensei+1
+	setCOsuperName          coAWDSKronii,ScriptAWDSSensei+2
+	setCOpowerText          coAWDSKronii,ScriptAWDSSensei+8,ScriptAWDSSensei+9,ScriptAWDSSensei+10,ScriptAWDSSensei+11,ScriptAWDSSensei+12,ScriptAWDSSensei+13
+	setCOprofileText        coAWDSKronii,ScriptAWDSSensei+3,ScriptAWDSSensei+4,ScriptAWDSSensei+5,ScriptAWDSSensei+6
+	setCOvictoryText        coAWDSKronii,ScriptAWDSSensei+7
 
-	setCOFaction			coAWDSSensei,ArmyYellowComet
-	setCOMusic          		coAWDSSensei,MusicSenseiTheme
+	setCOFaction			coAWDSKronii,ArmyYellowComet
+	setCOMusic          		coAWDSKronii,MusicKroniiTheme
 
-	setCOPowerCost          	coAWDSSensei,5
-	setCOSuperCost          	coAWDSSensei,10
+	setCOPowerCost          	coAWDSKronii,5
+	setCOSuperCost          	coAWDSKronii,10
 	
-	setCOD2DLandStat		coAWDSSensei,-30,0,0,0
-	setCOD2DNavyStat		coAWDSSensei,-50,0,0,0
-	setCOD2DTransportStat		coAWDSSensei,0,50,1,0
-	setCOD2DUnitStat		coAWDSSensei,Unit_BattleCopter,50,50,1,0
+	setCOD2DLandStat		coAWDSKronii,-30,0,0,0
+	setCOD2DNavyStat		coAWDSKronii,-50,0,0,0
+	setCOD2DTransportStat		coAWDSKronii,0,50,1,0
+	setCOD2DUnitStat		coAWDSKronii,Unit_BattleCopter,50,50,1,0
 
         setCOPowerLandStat		coAWDSSensei,-30,0,0,0
 	setCOPowerNavyStat		coAWDSSensei,-50,0,0,0
