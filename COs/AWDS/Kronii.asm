a913fc27cb28af2fd31cb490cc30bb1a8b6e541b
Zerom999 <maregee@hotmail.com>
2021-09-01 18:05:31 -0230

    Update Kronii.asm

--- COs/AWDS/Kronii.asm
+++ COs/AWDS/Kronii.asm
@@ -24,14 +24,17 @@ setCODefaults           coAWDSSensei
 	setCOPowerCost          	coAWDSSensei,5
 	setCOSuperCost          	coAWDSSensei,10
 	
+	setCOD2DLandStat		coAWDSSensei,-30,0,0,0
 	setCOD2DNavyStat		coAWDSSensei,-50,0,0,0
 	setCOD2DTransportStat		coAWDSSensei,0,50,1,0
 	setCOD2DUnitStat		coAWDSSensei,Unit_BattleCopter,50,50,1,0
 
+        setCOPowerLandStat		coAWDSSensei,-30,0,0,0
 	setCOPowerNavyStat		coAWDSSensei,-50,0,0,0
 	setCOPowerTransportStat		coAWDSSensei,0,70,1,0
 	setCOPowerUnitStat		coAWDSSensei,Unit_BattleCopter,70,70,1,0
-
+        
+	setCOSuperLandStat		coAWDSSensei,-30,0,0,0
 	setCOSuperNavyStat		coAWDSSensei,-50,0,0,0
 	setCOSuperTransportStat    	coAWDSSensei,0,90,1,0
 	setCOSuperUnitStat		coAWDSSensei,Unit_BattleCopter,90,90,1,0
