e5338accb9f207bfa59fc75b78ee600f66318cb4
Zerom999 <maregee@hotmail.com>
2021-09-02 16:06:18 -0230

    Update terrainboosterSubaru.asm

--- datatables/COStats/TerrainBoosters/terrainboosterSubaru.asm
+++ datatables/COStats/TerrainBoosters/terrainboosterSubaru.asm
@@ -6,11 +6,11 @@ TerrainFirepowerBoostSubaruDaytoDay:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,20,0,0
+	.db 0,30,0,0
 	;HQ, None, Airport, Port
 	.db 0,0,0,0
 	;Bridge, Shoal, Base, Pipeline
-	.db 10,0,0,0
+	.db 30,0,0,0
 	;Pipeseam, Silo, Silo - Launched, Reef
 	.db 0,0,0,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
@@ -24,11 +24,11 @@ TerrainFirepowerBoostSubaruPower:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,40,0,0
+	.db 0,60,0,0
 	;HQ, None, Airport, Port
 	.db 0,0,0,0
 	;Bridge, Shoal, Base, Pipeline
-	.db 20,0,0,0
+	.db 60,0,0,0
 	;Pipeseam, Silo, Silo - Launched, Reef
 	.db 0,0,0,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
@@ -42,11 +42,11 @@ TerrainFirepowerBoostSubaruSuper:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,60,0,0
+	.db 0,90,0,0
 	;HQ, None, Airport, Port
 	.db 0,0,0,0
 	;Bridge, Shoal, Base, Pipeline
-	.db 30,0,0,0
+	.db 90,0,0,0
 	;Pipeseam, Silo, Silo - Launched, Reef
 	.db 0,0,0,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
