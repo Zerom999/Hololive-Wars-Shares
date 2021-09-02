7959d41082858bea95f1e6bf2adfcb917ce1a9b3
Zerom999 <maregee@hotmail.com>
2021-09-02 17:37:37 -0230

    Update Baelz.asm

--- COs/AWDS/Baelz.asm
+++ COs/AWDS/Baelz.asm
@@ -11,12 +11,12 @@
 	setCOGFXSuperAnim       coAWDSBaelz,7,7
 	setCOGFXSuperSFX        coAWDSBaelz,0x1F4,0x1f5
 	
-	setCOName               coAWDSBaelz,ScriptAWDSOkayu
-	setCOpowerName          coAWDSBaelz,ScriptAWDSOkayu+1
-	setCOsuperName          coAWDSBaelz,ScriptAWDSOkayu+2
-	setCOpowerText          coAWDSBaelz,ScriptAWDSOkayu+8,ScriptAWDSOkayu+9,ScriptAWDSOkayu+10,ScriptAWDSOkayu+11,ScriptAWDSOkayu+12,ScriptAWDSOkayu+13
-	setCOprofileText        coAWDSBaelz,ScriptAWDSOkayu+3,ScriptAWDSOkayu+4,ScriptAWDSOkayu+5,ScriptAWDSOkayu+6
-	setCOvictoryText        coAWDSBaelz,ScriptAWDSOkayu+7
+	setCOName               coAWDSBaelz,ScriptAWDSBaelz
+	setCOpowerName          coAWDSBaelz,ScriptAWDSBaelz+1
+	setCOsuperName          coAWDSBaelz,ScriptAWDSBaelz+2
+	setCOpowerText          coAWDSBaelz,ScriptAWDSBaelz+8,ScriptAWDSBaelz+9,ScriptAWDSBaelz+10,ScriptAWDSBaelz+11,ScriptAWDSBaelz+12,ScriptAWDSBaelz+13
+	setCOprofileText        coAWDSBaelz,ScriptAWDSBaelz+3,ScriptAWDSBaelz+4,ScriptAWDSBaelz+5,ScriptAWDSBaelz+6
+	setCOvictoryText        coAWDSBaelz,ScriptAWDSBaelz+7
 
 	setCOFaction		coAWDSBaelz,ArmyGreenEarth
 	setCOMusic          	coAWDSBaelz,MusicBaelzTheme
