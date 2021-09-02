5c8142c67d077a8ff1443552c43703b23fa77a9e
Zerom999 <maregee@hotmail.com>
2021-09-02 17:36:07 -0230

    Update Baelz.asm

--- COs/AWDS/Baelz.asm
+++ COs/AWDS/Baelz.asm
@@ -1,11 +1,11 @@
         setCODefaults           coAWDSBaelz
 	setCOGameID		coAWDSBaelz,Game_AWDS
 
-	setCOGFXFullbody        coAWDSBaelz,GraphicsAWDSOkayuGraphic
-	setCOGFXName            coAWDSBaelz,GraphicsAWDSOkayuName
-	setCOGFXPalette         coAWDSBaelz,GraphicsAWDSOkayuPalette
-	setCOGFXFace            coAWDSBaelz,GraphicsAWDSOkayuFace,GraphicsAWDSOkayuHappyFace,GraphicsAWDSOkayuSadFace
-	setCOGFXSmallFace       coAWDSBaelz,GraphicsAWDSOkayuMugshot
+	setCOGFXFullbody        coAWDSBaelz,GraphicsAWDSBaelzGraphic
+	setCOGFXName            coAWDSBaelz,GraphicsAWDSBaelzName
+	setCOGFXPalette         coAWDSBaelz,GraphicsAWDSBaelzPalette
+	setCOGFXFace            coAWDSBaelz,GraphicsAWDSBaelzFace,GraphicsAWDSBaelz,HappyFace,GraphicsAWDSBaelzSadFace
+	setCOGFXSmallFace       coAWDSBaelz,GraphicsAWDSBaelzMugshot
 	setCOGFXPowerAnim       coAWDSBaelz,0,0
 	setCOGFXPowerSFX        coAWDSBaelz,0xB4,0xB4
 	setCOGFXSuperAnim       coAWDSBaelz,7,7
