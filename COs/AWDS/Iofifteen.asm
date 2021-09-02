83a799b3ccca716bbf6905311e1303c5bbf47446
Zerom999 <maregee@hotmail.com>
2021-09-01 21:36:44 -0230

    Update Iofifteen.asm

--- COs/AWDS/Iofifteen.asm
+++ COs/AWDS/Iofifteen.asm
@@ -1,22 +1,22 @@
         setCODefaults           coAWDSIofifteen
-	setCOGameID		coAWDSSonja,Game_AWDS
+	setCOGameID		coAWDSIofifteen,Game_AWDS
 
-	setCOGFXFullbody        coAWDSSonja,GraphicsAWDSSonjaGraphic
-	setCOGFXName            coAWDSSonja,GraphicsAWDSSonjaName
-	setCOGFXPalette         coAWDSSonja,GraphicsAWDSSonjaPalette
-	setCOGFXFace            coAWDSSonja,GraphicsAWDSSonjaFace,GraphicsAWDSSonjaHappyFace,GraphicsAWDSSonjaSadFace
-	setCOGFXSmallFace       coAWDSSonja,GraphicsAWDSSonjaMugshot
-	setCOGFXPowerAnim       coAWDSSonja,0,0
-	setCOGFXPowerSFX        coAWDSSonja,0xC4,0
-	setCOGFXSuperAnim       coAWDSSonja,7,7
-	setCOGFXSuperSFX        coAWDSSonja,0xC4,0
+	setCOGFXFullbody        coAWDSIofifteen,GraphicsAWDSSonjaGraphic
+	setCOGFXName            coAWDSIofifteen,GraphicsAWDSSonjaName
+	setCOGFXPalette         coAWDSIofifteen,GraphicsAWDSSonjaPalette
+	setCOGFXFace            coAWDSIofifteen,GraphicsAWDSSonjaFace,GraphicsAWDSSonjaHappyFace,GraphicsAWDSSonjaSadFace
+	setCOGFXSmallFace       coAWDSIofifteen,GraphicsAWDSSonjaMugshot
+	setCOGFXPowerAnim       coAWDSIofifteen,0,0
+	setCOGFXPowerSFX        coAWDSIofifteen,0xC4,0
+	setCOGFXSuperAnim       coAWDSIofifteen,7,7
+	setCOGFXSuperSFX        coAWDSIofifteen,0xC4,0
 	
-	setCOName               coAWDSSonja,ScriptAWDSSonja
-	setCOpowerName          coAWDSSonja,ScriptAWDSSonja+1
-	setCOsuperName          coAWDSSonja,ScriptAWDSSonja+2
-	setCOpowerText          coAWDSSonja,ScriptAWDSSonja+8,ScriptAWDSSonja+9,ScriptAWDSSonja+10,ScriptAWDSSonja+11,ScriptAWDSSonja+12,ScriptAWDSSonja+13
-	setCOprofileText        coAWDSSonja,ScriptAWDSSonja+3,ScriptAWDSSonja+4,ScriptAWDSSonja+5,ScriptAWDSSonja+6
-	setCOvictoryText        coAWDSSonja,ScriptAWDSSonja+7
+	setCOName               coAWDSIofifteen,ScriptAWDSSonja
+	setCOpowerName          coAWDSIofifteen,ScriptAWDSSonja+1
+	setCOsuperName          coAWDSIofifteen,ScriptAWDSSonja+2
+	setCOpowerText          coAWDSIofifteen,ScriptAWDSSonja+8,ScriptAWDSSonja+9,ScriptAWDSSonja+10,ScriptAWDSSonja+11,ScriptAWDSSonja+12,ScriptAWDSSonja+13
+	setCOprofileText        coAWDSIofifteen,ScriptAWDSSonja+3,ScriptAWDSSonja+4,ScriptAWDSSonja+5,ScriptAWDSSonja+6
+	setCOvictoryText        coAWDSIofifteen,ScriptAWDSSonja+7
 
 	setCOFaction		coAWDSSonja,ArmyYellowComet
 	setCOMusic          	coAWDSSonja,MusicSonjaTheme
