6b6b6cec0b427dc38e5670f8a8ffba53f50d9cb3
Zerom999 <maregee@hotmail.com>
2021-09-04 12:56:56 -0230

    Update Artia.asm

--- COs/AWDS/Artia.asm
+++ COs/AWDS/Artia.asm
@@ -1,1 +1,39 @@
+setCODefaults           coAWDSFlak
+	setCOGameID		coAWDSFlak,Game_AWDS
+
+	setCOGFXFullbody        coAWDSFlak,GraphicsAWDSFlakGraphic
+	setCOGFXName            coAWDSFlak,GraphicsAWDSFlakName
+	setCOGFXPalette         coAWDSFlak,GraphicsAWDSFlakPalette
+	setCOGFXFace            coAWDSFlak,GraphicsAWDSFlakFace,GraphicsAWDSFlakHappyFace,GraphicsAWDSFlakSadFace
+	setCOGFXSmallFace       coAWDSFlak,GraphicsAWDSFlakMugshot
+	setCOGFXPowerAnim       coAWDSFlak,0,0
+	setCOGFXPowerSFX        coAWDSFlak,0xbd,0xbe
+	setCOGFXSuperAnim       coAWDSFlak,7,7
+	setCOGFXSuperSFX        coAWDSFlak,0x1f4,0x1f5
+	
+	setCOName               coAWDSFlak,ScriptAWDSFlak
+	setCOpowerName          coAWDSFlak,ScriptAWDSFlak+1
+	setCOsuperName          coAWDSFlak,ScriptAWDSFlak+2
+	setCOpowerText          coAWDSFlak,ScriptAWDSFlak+8,ScriptAWDSFlak+9,ScriptAWDSFlak+10,ScriptAWDSFlak+11,ScriptAWDSFlak+12,ScriptAWDSFlak+13
+	setCOprofileText        coAWDSFlak,ScriptAWDSFlak+3,ScriptAWDSFlak+4,ScriptAWDSFlak+5,ScriptAWDSFlak+6
+	setCOvictoryText        coAWDSFlak,ScriptAWDSFlak+7
+
+	setCOFaction		coAWDSFlak,ArmyBlackHole
+	setCOMusic          	coAWDSFlak,MusicFlakTheme
+	setCOPowerMusic         coAWDSFlak,MusicBlackPower
+	setCOSuperMusic         coAWDSFlak,MusicBlackSuper
+
+	setCOPowerCost          coAWDSFlak,3
+	setCOSuperCost          coAWDSFlak,6
+
+	setCOD2DLuck		coAWDSFlak,30
+	setCOD2DBadLuck		coAWDSFlak,15
+	setCOPowerLuck		coAWDSFlak,70
+	setCOPowerBadLuck	coAWDSFlak,35
+	setCOSuperLuck		coAWDSFlak,110
+	setCOSuperBadLuck	coAWDSFlak,55
+	
+	setCOAIPowerRange       coAWDSFlak,25
+	setCOAIPowerTrigger     coAWDSFlak,95
+; blank line
 
