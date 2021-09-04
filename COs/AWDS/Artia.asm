237dbca87735ccc3bfdd501c438e1c38a8ed6bcc
Zerom999 <maregee@hotmail.com>
2021-09-04 13:00:26 -0230

    Update Artia.asm

--- COs/AWDS/Artia.asm
+++ COs/AWDS/Artia.asm
@@ -1,41 +1,41 @@
         setCODefaults           coAWDSArtia
-	setCOGameID		coAWDSFlak,Game_AWDS
+	setCOGameID		coAWDSArtia,Game_AWDS
 
-	setCOGFXFullbody        coAWDSFlak,GraphicsAWDSFlakGraphic
-	setCOGFXName            coAWDSFlak,GraphicsAWDSFlakName
-	setCOGFXPalette         coAWDSFlak,GraphicsAWDSFlakPalette
-	setCOGFXFace            coAWDSFlak,GraphicsAWDSFlakFace,GraphicsAWDSFlakHappyFace,GraphicsAWDSFlakSadFace
-	setCOGFXSmallFace       coAWDSFlak,GraphicsAWDSFlakMugshot
-	setCOGFXPowerAnim       coAWDSFlak,0,0
-	setCOGFXPowerSFX        coAWDSFlak,0xbd,0xbe
-	setCOGFXSuperAnim       coAWDSFlak,7,7
-	setCOGFXSuperSFX        coAWDSFlak,0x1f4,0x1f5
+	setCOGFXFullbody        coAWDSArtia,GraphicsAWDSFlakGraphic
+	setCOGFXName            coAWDSArtia,GraphicsAWDSFlakName
+	setCOGFXPalette         coAWDSArtia,GraphicsAWDSFlakPalette
+	setCOGFXFace            coAWDSArtia,GraphicsAWDSFlakFace,GraphicsAWDSFlakHappyFace,GraphicsAWDSFlakSadFace
+	setCOGFXSmallFace       coAWDSArtia,GraphicsAWDSFlakMugshot
+	setCOGFXPowerAnim       coAWDSArtia,0,0
+	setCOGFXPowerSFX        coAWDSArtia,0xbd,0xbe
+	setCOGFXSuperAnim       coAWDSArtia,7,7
+	setCOGFXSuperSFX        coAWDSArtia,0x1f4,0x1f5
 	
-	setCOName               coAWDSFlak,ScriptAWDSFlak
-	setCOpowerName          coAWDSFlak,ScriptAWDSFlak+1
-	setCOsuperName          coAWDSFlak,ScriptAWDSFlak+2
-	setCOpowerText          coAWDSFlak,ScriptAWDSFlak+8,ScriptAWDSFlak+9,ScriptAWDSFlak+10,ScriptAWDSFlak+11,ScriptAWDSFlak+12,ScriptAWDSFlak+13
-	setCOprofileText        coAWDSFlak,ScriptAWDSFlak+3,ScriptAWDSFlak+4,ScriptAWDSFlak+5,ScriptAWDSFlak+6
-	setCOvictoryText        coAWDSFlak,ScriptAWDSFlak+7
+	setCOName               coAWDSArtia,ScriptAWDSFlak
+	setCOpowerName          coAWDSArtia,ScriptAWDSFlak+1
+	setCOsuperName          coAWDSArtia,ScriptAWDSFlak+2
+	setCOpowerText          coAWDSArtia,ScriptAWDSFlak+8,ScriptAWDSFlak+9,ScriptAWDSFlak+10,ScriptAWDSFlak+11,ScriptAWDSFlak+12,ScriptAWDSFlak+13
+	setCOprofileText        coAWDSArtia,ScriptAWDSFlak+3,ScriptAWDSFlak+4,ScriptAWDSFlak+5,ScriptAWDSFlak+6
+	setCOvictoryText        coAWDSArtia,ScriptAWDSFlak+7
 
-	setCOFaction		coAWDSFlak,ArmyBlackHole
-	setCOMusic          	coAWDSFlak,MusicFlakTheme
-	setCOPowerMusic         coAWDSFlak,MusicBlackPower
-	setCOSuperMusic         coAWDSFlak,MusicBlackSuper
+	setCOFaction		coAWDSArtia,ArmyBlackHole
+	setCOMusic          	coAWDSArtia,MusicFlakTheme
+	setCOPowerMusic         coAWDSArtia,MusicBlackPower
+	setCOSuperMusic         coAWDSArtia,MusicBlackSuper
 
-	setCOPowerCost          coAWDSFlak,3
-	setCOSuperCost          coAWDSFlak,6
+	setCOPowerCost          coAWDSArtia,3
+	setCOSuperCost          coAWDSArtia,6
 
-	setCOD2DSnowFirepower	coAWDSFubuki,20
-	setCOPowerSnowFirepower	coAWDSFubuki,40
-	setCOSuperSnowFirepower	coAWDSFubuki,60
+	setCOD2DSnowFirepower	coAWDSArtia,20
+	setCOPowerSnowFirepower	coAWDSArtia,40
+	setCOSuperSnowFirepower	coAWDSArtia,60
 	
-	setCOD2DMove		coAWDSFubuki, MovementChartClear,MovementChartClear,MovementChartSnow
-	setCOPowerMove		coAWDSFubuki, MovementChartClear,MovementChartClear,MovementChartSnow
-	setCOSuperMove		coAWDSFubuki, MovementChartClear,MovementChartClear,MovementChartSnow
+	setCOD2DMove		coAWDSArtia, MovementChartClear,MovementChartClear,MovementChartSnow
+	setCOPowerMove		coAWDSArtia, MovementChartClear,MovementChartClear,MovementChartSnow
+	setCOSuperMove		coAWDSArtia, MovementChartClear,MovementChartClear,MovementChartSnow
 	
 	
-	setCOAIPowerRange       coAWDSFlak,25
-	setCOAIPowerTrigger     coAWDSFlak,95
+	setCOAIPowerRange       coAWDSArtia,25
+	setCOAIPowerTrigger     coAWDSArtia,95
 ; blank line
 
