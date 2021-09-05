3c1075b3086a00d0da6421c92e65163e70f58d17
Zerom999 <maregee@hotmail.com>
2021-09-05 19:03:15 -0230

    Update terrainboosterDorisdaily.asm

--- datatables/COStats/TerrainBoosters/terrainboosterDorisdaily.asm
+++ datatables/COStats/TerrainBoosters/terrainboosterDorisdaily.asm
@@ -6,11 +6,11 @@ TerrainFirepowerBoostKoalDaytoDay:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,20,0,0
+	.db 0,0,0,0
 	;HQ, None, Airport, Port
 	.db 0,0,0,0
 	;Bridge, Shoal, Base, Pipeline
-	.db 10,0,0,0
+	.db 0,0,0,0
 	;Pipeseam, Silo, Silo - Launched, Reef
 	.db 0,0,0,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
@@ -24,11 +24,11 @@ TerrainFirepowerBoostDorisPower:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,40,0,0
+	.db 0,0,0,0
 	;HQ, None, Airport, Port
 	.db 0,0,0,0
 	;Bridge, Shoal, Base, Pipeline
-	.db 20,0,0,0
+	.db 0,0,0,0
 	;Pipeseam, Silo, Silo - Launched, Reef
 	.db 0,0,0,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
@@ -42,11 +42,11 @@ TerrainFirepowerBoostDorisSuper:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,60,0,0
+	.db 0,0,0,0
 	;HQ, None, Airport, Port
 	.db 0,0,0,0
 	;Bridge, Shoal, Base, Pipeline
-	.db 30,0,0,0
+	.db 0,0,0,0
 	;Pipeseam, Silo, Silo - Launched, Reef
 	.db 0,0,0,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
