9831bdbb11f8326f98caeda4b85906ba8e49a24b
Zerom999 <maregee@hotmail.com>
2021-09-05 19:03:55 -0230

    Update terrainboosterDorisdaily.asm

--- datatables/COStats/TerrainBoosters/terrainboosterDorisdaily.asm
+++ datatables/COStats/TerrainBoosters/terrainboosterDorisdaily.asm
@@ -4,15 +4,15 @@
 
 TerrainFirepowerBoostKoalDaytoDay:
 	;Null, Plains, River, Mountain
-	.db 0,0,0,0
+	.db 0,0,20,0
 	;Forest, Road, City, Sea
-	.db 0,0,0,0
+	.db 0,0,0,20
 	;HQ, None, Airport, Port
 	.db 0,0,0,0
 	;Bridge, Shoal, Base, Pipeline
-	.db 0,0,0,0
+	.db 0,20,0,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	.db 0,0,0,0
+	.db 0,0,0,20
 	;Lab, MinicannonS, MinicannonN, MinicannonW
 	.db 0,0,0,0
 	;MinicannonE, Laser, BlackCannonS, BlackCannonN
