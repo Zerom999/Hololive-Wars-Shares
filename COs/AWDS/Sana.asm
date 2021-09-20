5ec5a10841d97c55a00c045b266fca17007eb1d4
Zerom999 <maregee@hotmail.com>
2021-09-20 12:45:58 -0400

    Update Sana.asm

--- COs/AWDS/Sana.asm
+++ COs/AWDS/Sana.asm
@@ -1,1 +1,42 @@
+setCODefaults           coAWDSLamy
+	setCOGameID		coAWDSLamy,Game_AWDS
+
+	setCOGFXFullbody        coAWDSLamy,GraphicsAWDSLamyGraphic
+	setCOGFXName            coAWDSLamy,GraphicsAWDSLamyName
+	setCOGFXPalette         coAWDSLamy,GraphicsAWDSLamyPalette
+	setCOGFXFace            coAWDSLamy,GraphicsAWDSLamyFace,GraphicsAWDSLamyHappyFace,GraphicsAWDSLamySadFace
+	setCOGFXSmallFace       coAWDSLamy,GraphicsAWDSLamyMugshot
+	setCOGFXPowerAnim       coAWDSLamy,0,0
+	setCOGFXPowerSFX        coAWDSLamy,0xC3,0
+	setCOGFXSuperAnim       coAWDSLamy,7,7
+	setCOGFXSuperSFX        coAWDSLamy,0x1f7,0x1f8
+	
+	setCOName               coAWDSLamy,ScriptAWDSLamy
+	setCOpowerName          coAWDSLamy,ScriptAWDSLamy+1
+	setCOsuperName          coAWDSLamy,ScriptAWDSLamy+2
+	setCOpowerText          coAWDSLamy,ScriptAWDSLamy+8,ScriptAWDSLamy+9,ScriptAWDSLamy+10,ScriptAWDSLamy+11,ScriptAWDSLamy+12,ScriptAWDSLamy+13
+	setCOprofileText        coAWDSLamy,ScriptAWDSLamy+3,ScriptAWDSLamy+4,ScriptAWDSLamy+5,ScriptAWDSLamy+6
+	setCOvictoryText        coAWDSLamy,ScriptAWDSLamy+7
+
+	setCOFaction		coAWDSLamy,ArmyBlueMoon
+	setCOMusic          	coAWDSLamy,MusicLamyTheme
+
+	setCOPowerCost          coAWDSLamy,2
+	setCOSuperCost          coAWDSLamy,4
+
+	setCOD2DSnowFirepower	coAWDSLamy,20
+	setCOPowerSnowFirepower	coAWDSLamy,40
+	setCOSuperSnowFirepower	coAWDSLamy,60
+	
+	setCOD2DMove		coAWDSLamy, MovementChartClear,MovementChartClear,MovementChartSnow
+	setCOPowerMove		coAWDSLamy, MovementChartClear,MovementChartClear,MovementChartSnow
+	setCOSuperMove		coAWDSLamy, MovementChartClear,MovementChartClear,MovementChartSnow
+	
+	setCOPowerASM		coAWDSLamy,COPowerBlizzard
+	setCOSuperASM		coAWDSLamy,COPowerExtendedBlizzard
+	
+	setCOAIPowerRange       coAWDSLamy,15
+	setCOAIPowerTrigger     coAWDSLamy,70
+	setCOAIPowerTriggerASM	coAWDSLamy,AIActivateNotSnow
+; blank line
 
