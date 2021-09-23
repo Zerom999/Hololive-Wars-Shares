78f9a1b23ad8b2aa55fe1924dfad6a275566aab3
Zerom999 <maregee@hotmail.com>
2021-09-23 15:36:43 -0400

    Update Sana.asm

--- COs/AWDS/Sana.asm
+++ COs/AWDS/Sana.asm
@@ -1,25 +1,25 @@
         setCODefaults           coAWDSSana
 	setCOGameID		coAWDSSana,Game_AWDS
 
-	setCOGFXFullbody        coAWDSSana,GraphicsAWDSLamyGraphic
-	setCOGFXName            coAWDSSana,GraphicsAWDSLamyName
-	setCOGFXPalette         coAWDSSana,GraphicsAWDSLamyPalette
-	setCOGFXFace            coAWDSSana,GraphicsAWDSLamyFace,GraphicsAWDSLamyHappyFace,GraphicsAWDSLamySadFace
-	setCOGFXSmallFace       coAWDSSana,GraphicsAWDSLamyMugshot
+	setCOGFXFullbody        coAWDSSana,GraphicsAWDSSanaGraphic
+	setCOGFXName            coAWDSSana,GraphicsAWDSSanaName
+	setCOGFXPalette         coAWDSSana,GraphicsAWDSSanaPalette
+	setCOGFXFace            coAWDSSana,GraphicsAWDSSanaFace,GraphicsAWDSSanaHappyFace,GraphicsAWDSSanaSadFace
+	setCOGFXSmallFace       coAWDSSana,GraphicsAWDSSanaMugshot
 	setCOGFXPowerAnim       coAWDSSana,0,0
 	setCOGFXPowerSFX        coAWDSSana,0xC3,0
 	setCOGFXSuperAnim       coAWDSSana,7,7
 	setCOGFXSuperSFX        coAWDSSana,0x1f7,0x1f8
 	
-	setCOName               coAWDSSana,ScriptAWDSLamy
-	setCOpowerName          coAWDSSana,ScriptAWDSLamy+1
-	setCOsuperName          coAWDSSana,ScriptAWDSLamy+2
-	setCOpowerText          coAWDSSana,ScriptAWDSLamy+8,ScriptAWDSLamy+9,ScriptAWDSLamy+10,ScriptAWDSLamy+11,ScriptAWDSLamy+12,ScriptAWDSLamy+13
-	setCOprofileText        coAWDSSana,ScriptAWDSLamy+3,ScriptAWDSLamy+4,ScriptAWDSLamy+5,ScriptAWDSLamy+6
-	setCOvictoryText        coAWDSSana,ScriptAWDSLamy+7
+	setCOName               coAWDSSana,ScriptAWDSSana
+	setCOpowerName          coAWDSSana,ScriptAWDSSana+1
+	setCOsuperName          coAWDSSana,ScriptAWDSSana+2
+	setCOpowerText          coAWDSSana,ScriptAWDSSana+8,ScriptAWDSSana+9,ScriptAWDSSana+10,ScriptAWDSSana+11,ScriptAWDSSana+12,ScriptAWDSSana+13
+	setCOprofileText        coAWDSSana,ScriptAWDSSana+3,ScriptAWDSSana+4,ScriptAWDSSana+5,ScriptAWDSSana+6
+	setCOvictoryText        coAWDSSana,ScriptAWDSSana+7
 
-	setCOFaction		coAWDSSana,ArmyBlueMoon
-	setCOMusic          	coAWDSSana,MusicLamyTheme
+	setCOFaction		coAWDSSana,ArmyGreenEarth
+	setCOMusic          	coAWDSSana,MusicSanaTheme
 
 	setCOPowerCost          coAWDSSana,2
 	setCOSuperCost          coAWDSSana,4
