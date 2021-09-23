57750cce2fa733f02ffe464aaaba1fe85bc6e218
Zerom999 <maregee@hotmail.com>
2021-09-23 15:33:16 -0400

    Update Sana.asm

--- COs/AWDS/Sana.asm
+++ COs/AWDS/Sana.asm
@@ -11,23 +11,20 @@
 	setCOGFXSuperAnim       coAWDSSana,7,7
 	setCOGFXSuperSFX        coAWDSSana,0x1f7,0x1f8
 	
-	setCOName               coAWDSLamy,ScriptAWDSLamy
-	setCOpowerName          coAWDSLamy,ScriptAWDSLamy+1
-	setCOsuperName          coAWDSLamy,ScriptAWDSLamy+2
-	setCOpowerText          coAWDSLamy,ScriptAWDSLamy+8,ScriptAWDSLamy+9,ScriptAWDSLamy+10,ScriptAWDSLamy+11,ScriptAWDSLamy+12,ScriptAWDSLamy+13
-	setCOprofileText        coAWDSLamy,ScriptAWDSLamy+3,ScriptAWDSLamy+4,ScriptAWDSLamy+5,ScriptAWDSLamy+6
-	setCOvictoryText        coAWDSLamy,ScriptAWDSLamy+7
+	setCOName               coAWDSSana,ScriptAWDSLamy
+	setCOpowerName          coAWDSSana,ScriptAWDSLamy+1
+	setCOsuperName          coAWDSSana,ScriptAWDSLamy+2
+	setCOpowerText          coAWDSSana,ScriptAWDSLamy+8,ScriptAWDSLamy+9,ScriptAWDSLamy+10,ScriptAWDSLamy+11,ScriptAWDSLamy+12,ScriptAWDSLamy+13
+	setCOprofileText        coAWDSSana,ScriptAWDSLamy+3,ScriptAWDSLamy+4,ScriptAWDSLamy+5,ScriptAWDSLamy+6
+	setCOvictoryText        coAWDSSana,ScriptAWDSLamy+7
 
-	setCOFaction		coAWDSLamy,ArmyBlueMoon
-	setCOMusic          	coAWDSLamy,MusicLamyTheme
+	setCOFaction		coAWDSSana,ArmyBlueMoon
+	setCOMusic          	coAWDSSana,MusicLamyTheme
 
-	setCOPowerCost          coAWDSLamy,2
-	setCOSuperCost          coAWDSLamy,4
+	setCOPowerCost          coAWDSSana,2
+	setCOSuperCost          coAWDSSana,4
 
-	setCOD2DSnowFirepower	coAWDSLamy,20
-	setCOPowerSnowFirepower	coAWDSLamy,40
-	setCOSuperSnowFirepower	coAWDSLamy,60
-
+	
 	setCOPowerASM		coAWDSLamy,COPowerBlizzard
 	setCOSuperASM		coAWDSLamy,COPowerExtendedBlizzard
 	
