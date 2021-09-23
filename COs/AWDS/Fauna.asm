a329f9d753f293338ef1cb6ae2eb7ce818e9e4fa
Zerom999 <maregee@hotmail.com>
2021-09-23 15:49:47 -0400

    Update Fauna.asm

--- COs/AWDS/Fauna.asm
+++ COs/AWDS/Fauna.asm
@@ -1,1 +1,40 @@
+setCODefaults           coAWDSWatame
+	setCOGameID		coAWDSWatame,Game_AWDS
+
+	setCOGFXFullbody        coAWDSWatame,GraphicsAWDSWatameGraphic
+	setCOGFXName            coAWDSWatame,GraphicsAWDSWatameName
+	setCOGFXPalette         coAWDSWatame,GraphicsAWDSWatamePalette
+	setCOGFXFace            coAWDSWatame,GraphicsAWDSWatameFace,GraphicsAWDSWatameHappyFace,GraphicsAWDSWatameSadFace
+	setCOGFXSmallFace       coAWDSWatame,GraphicsAWDSWatameMugshot
+	setCOGFXPowerAnim       coAWDSWatame,2,0
+	setCOGFXPowerSFX        coAWDSWatame,0xb7,0xb8
+	setCOGFXSuperAnim       coAWDSWatame,7,7
+	setCOGFXSuperSFX        coAWDSWatame,0x1f7,0x1f8
+	
+	setCOName               coAWDSWatame,ScriptAWDSWatame
+	setCOpowerName          coAWDSWatame,ScriptAWDSWatame+1
+	setCOsuperName          coAWDSWatame,ScriptAWDSWatame+2
+	setCOpowerText          coAWDSWatame,ScriptAWDSWatame+8,ScriptAWDSWatame+9,ScriptAWDSWatame+10,ScriptAWDSWatame+11,ScriptAWDSWatame+12,ScriptAWDSWatame+13
+	setCOprofileText        coAWDSWatame,ScriptAWDSWatame+3,ScriptAWDSWatame+4,ScriptAWDSWatame+5,ScriptAWDSWatame+6
+	setCOvictoryText        coAWDSWatame,ScriptAWDSWatame+7
+
+	setCOFaction		coAWDSWatame,ArmyYellowComet
+	setCOMusic          	coAWDSWatame,MusicWatameTheme
+
+	setCOPowerCost          coAWDSWatame,3
+	setCOSuperCost          coAWDSWatame,6
+
+	setCOD2DTerrainFirepowerList	coAWDSWatame,TerrainFirepowerBoostWatameDaytoDay
+	
+	setCOPowerLandStat		coAWDSWatame,0,0,1,0
+	setCOPowerIndirectLandStat	coAWDSWatame,0,0,1,0
+	setCOPowerTerrainFirepowerList	coAWDSWatame,TerrainFirepowerBoostWatamePower
+	
+	setCOSuperLandStat		coAWDSWatame,0,0,2,0
+	setCOSuperIndirectLandStat	coAWDSWatame,0,0,2,0
+	setCOSuperTerrainFirepowerList	coAWDSWatame,TerrainFirepowerBoostWatameSuper
+	
+	setCOAIPowerRange       coAWDSWatame,25
+	setCOAIPowerTrigger     coAWDSWatame,95
+; blank line
 
