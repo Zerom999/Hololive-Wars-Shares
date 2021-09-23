c47acb2344282282b922f157e279b7dd9cbfa6de
Zerom999 <maregee@hotmail.com>
2021-09-23 15:51:22 -0400

    Update Fauna.asm

--- COs/AWDS/Fauna.asm
+++ COs/AWDS/Fauna.asm
@@ -1,22 +1,22 @@
-        setCODefaults           coAWDSWatame
-	setCOGameID		coAWDSWatame,Game_AWDS
+        setCODefaults           coAWDSFauna
+	setCOGameID		coAWDSFauna,Game_AWDS
 
-	setCOGFXFullbody        coAWDSWatame,GraphicsAWDSWatameGraphic
-	setCOGFXName            coAWDSWatame,GraphicsAWDSWatameName
-	setCOGFXPalette         coAWDSWatame,GraphicsAWDSWatamePalette
-	setCOGFXFace            coAWDSWatame,GraphicsAWDSWatameFace,GraphicsAWDSWatameHappyFace,GraphicsAWDSWatameSadFace
-	setCOGFXSmallFace       coAWDSWatame,GraphicsAWDSWatameMugshot
-	setCOGFXPowerAnim       coAWDSWatame,2,0
-	setCOGFXPowerSFX        coAWDSWatame,0xb7,0xb8
-	setCOGFXSuperAnim       coAWDSWatame,7,7
-	setCOGFXSuperSFX        coAWDSWatame,0x1f7,0x1f8
+	setCOGFXFullbody        coAWDSFauna,GraphicsAWDSWatameGraphic
+	setCOGFXName            coAWDSFauna,GraphicsAWDSWatameName
+	setCOGFXPalette         coAWDSFauna,GraphicsAWDSWatamePalette
+	setCOGFXFace            coAWDSFauna,GraphicsAWDSWatameFace,GraphicsAWDSWatameHappyFace,GraphicsAWDSWatameSadFace
+	setCOGFXSmallFace       coAWDSFauna,GraphicsAWDSWatameMugshot
+	setCOGFXPowerAnim       coAWDSFauna,2,0
+	setCOGFXPowerSFX        coAWDSFauna,0xb7,0xb8
+	setCOGFXSuperAnim       coAWDSFauna,7,7
+	setCOGFXSuperSFX        coAWDSFauna,0x1f7,0x1f8
 	
-	setCOName               coAWDSWatame,ScriptAWDSWatame
-	setCOpowerName          coAWDSWatame,ScriptAWDSWatame+1
-	setCOsuperName          coAWDSWatame,ScriptAWDSWatame+2
-	setCOpowerText          coAWDSWatame,ScriptAWDSWatame+8,ScriptAWDSWatame+9,ScriptAWDSWatame+10,ScriptAWDSWatame+11,ScriptAWDSWatame+12,ScriptAWDSWatame+13
-	setCOprofileText        coAWDSWatame,ScriptAWDSWatame+3,ScriptAWDSWatame+4,ScriptAWDSWatame+5,ScriptAWDSWatame+6
-	setCOvictoryText        coAWDSWatame,ScriptAWDSWatame+7
+	setCOName               coAWDSFauna,ScriptAWDSWatame
+	setCOpowerName          coAWDSFauna,ScriptAWDSWatame+1
+	setCOsuperName          coAWDSFauna,ScriptAWDSWatame+2
+	setCOpowerText          coAWDSFauna,ScriptAWDSWatame+8,ScriptAWDSWatame+9,ScriptAWDSWatame+10,ScriptAWDSWatame+11,ScriptAWDSWatame+12,ScriptAWDSWatame+13
+	setCOprofileText        coAWDSFauna,ScriptAWDSWatame+3,ScriptAWDSWatame+4,ScriptAWDSWatame+5,ScriptAWDSWatame+6
+	setCOvictoryText        coAWDSFauna,ScriptAWDSWatame+7
 
 	setCOFaction		coAWDSWatame,ArmyYellowComet
 	setCOMusic          	coAWDSWatame,MusicWatameTheme
