85218c2122a2603ea8e8d3100e709646bfb23525
Zerom999 <maregee@hotmail.com>
2021-09-23 21:36:43 -0400

    Update terrainboosterFaunadaily.asm

--- datatables/COStats/TerrainBoosters/terrainboosterFaunadaily.asm
+++ datatables/COStats/TerrainBoosters/terrainboosterFaunadaily.asm
@@ -4,17 +4,17 @@
 
 TerrainFirepowerBoostFaunaDaytoDay:
 	;Null, Plains, River, Mountain
-	.db 0,20,0,0
+	.db 0,10,0,40
 	;Forest, Road, City, Sea
-	.db 0,0,0,0
+	.db 20,0,30,0
 	;HQ, None, Airport, Port
-	.db 0,0,0,0
+	.db 40,0,30,30
 	;Bridge, Shoal, Base, Pipeline
-	.db 0,0,0,0
+	.db 0,10,30,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	.db 0,0,0,0
+	.db 0,30,30,10
 	;Lab, MinicannonS, MinicannonN, MinicannonW
-	.db 0,0,0,0
+	.db 30,0,0,0
 	;MinicannonE, Laser, BlackCannonS, BlackCannonN
 	.db 0,0,0,0
 	;Volcano, Factory, Deathray, Variable Tile
@@ -22,17 +22,17 @@ TerrainFirepowerBoostFaunaDaytoDay:
 	
 TerrainFirepowerBoostFaunaPower:
 	;Null, Plains, River, Mountain
-	.db 0,40,0,0
+	.db 0,20,0,80
 	;Forest, Road, City, Sea
-	.db 0,0,0,0
+	.db 40,0,60,0
 	;HQ, None, Airport, Port
-	.db 0,0,0,0
+	.db 80,0,60,60
 	;Bridge, Shoal, Base, Pipeline
-	.db 0,0,0,0
+	.db 0,20,60,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	.db 0,0,0,0
+	.db 0,60,60,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
-	.db 0,0,0,0
+	.db 60,0,0,0
 	;MinicannonE, Laser, BlackCannonS, BlackCannonN
 	.db 0,0,0,0
 	;Volcano, Factory, Deathray, Variable Tile
@@ -40,17 +40,17 @@ TerrainFirepowerBoostFaunaPower:
 	
 TerrainFirepowerBoostFaunaSuper:
 	;Null, Plains, River, Mountain
-	.db 0,60,0,0
+	.db 0,30,0,120
 	;Forest, Road, City, Sea
-	.db 0,0,0,0
+	.db 60,0,90,0
 	;HQ, None, Airport, Port
-	.db 0,0,0,0
+	.db 120,0,90,90
 	;Bridge, Shoal, Base, Pipeline
-	.db 0,0,0,0
+	.db 0,30,90,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	.db 0,0,0,0
+	.db 0,90,90,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
-	.db 0,0,0,0
+	.db 90,0,0,0
 	;MinicannonE, Laser, BlackCannonS, BlackCannonN
 	.db 0,0,0,0
 	;Volcano, Factory, Deathray, Variable Tile
