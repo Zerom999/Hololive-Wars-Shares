a50e4577513562dcc67dda1dd47466370fe54eaf
Zerom999 <maregee@hotmail.com>
2021-09-23 21:33:52 -0400

    Create terrainboosterFaunadaily.asm

---
+++ datatables/COStats/TerrainBoosters/terrainboosterFaunadaily.asm
@@ -0,0 +1,58 @@
+;terrainboosterFaunadaily.asm
+
+;Terrain Boosting Table
+
+TerrainFirepowerBoostFaunaDaytoDay:
+	;Null, Plains, River, Mountain
+	.db 0,20,0,0
+	;Forest, Road, City, Sea
+	.db 0,0,0,0
+	;HQ, None, Airport, Port
+	.db 0,0,0,0
+	;Bridge, Shoal, Base, Pipeline
+	.db 0,0,0,0
+	;Pipeseam, Silo, Silo - Launched, Reef
+	.db 0,0,0,0
+	;Lab, MinicannonS, MinicannonN, MinicannonW
+	.db 0,0,0,0
+	;MinicannonE, Laser, BlackCannonS, BlackCannonN
+	.db 0,0,0,0
+	;Volcano, Factory, Deathray, Variable Tile
+	.db 0,0,0,0
+	
+TerrainFirepowerBoostFaunaPower:
+	;Null, Plains, River, Mountain
+	.db 0,40,0,0
+	;Forest, Road, City, Sea
+	.db 0,0,0,0
+	;HQ, None, Airport, Port
+	.db 0,0,0,0
+	;Bridge, Shoal, Base, Pipeline
+	.db 0,0,0,0
+	;Pipeseam, Silo, Silo - Launched, Reef
+	.db 0,0,0,0
+	;Lab, MinicannonS, MinicannonN, MinicannonW
+	.db 0,0,0,0
+	;MinicannonE, Laser, BlackCannonS, BlackCannonN
+	.db 0,0,0,0
+	;Volcano, Factory, Deathray, Variable Tile
+	.db 0,0,0,0	
+	
+TerrainFirepowerBoostFaunaSuper:
+	;Null, Plains, River, Mountain
+	.db 0,60,0,0
+	;Forest, Road, City, Sea
+	.db 0,0,0,0
+	;HQ, None, Airport, Port
+	.db 0,0,0,0
+	;Bridge, Shoal, Base, Pipeline
+	.db 0,0,0,0
+	;Pipeseam, Silo, Silo - Launched, Reef
+	.db 0,0,0,0
+	;Lab, MinicannonS, MinicannonN, MinicannonW
+	.db 0,0,0,0
+	;MinicannonE, Laser, BlackCannonS, BlackCannonN
+	.db 0,0,0,0
+	;Volcano, Factory, Deathray, Variable Tile
+	.db 0,0,0,0	
+;Blank Line
