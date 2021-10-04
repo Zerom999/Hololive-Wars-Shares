1dd8c175a22b2586eb868db6f0a9b9cd83141cee
Zerom999 <maregee@hotmail.com>
2021-10-04 06:45:02 -0700

    Update and rename terrainboosterRobocopower.asm to terrainboosterRosaylnpower.asm

--- datatables/COStats/TerrainBoosters/terrainboosterRosaylnpower.asm
+++ datatables/COStats/TerrainBoosters/terrainboosterRosaylnpower.asm
@@ -2,19 +2,19 @@
 
 ;Terrain Boosting Table
 
-TerrainFirepowerBoostRobocoPower:
+TerrainFirepowerBoostRosaylnPower:
 	;Null, Plains, River, Mountain
 	;.db 0,0,0,0
 	;Forest, Road, City, Sea
-	;.db 0,0,90,0
+	;.db 0,0,80,0
 	;HQ, None, Airport, Port
-	;.db 90,0,90,90
+	;.db 80,0,80,80
 	;Bridge, Shoal, Base, Pipeline
-	;.db 0,0,90,0
+	;.db 0,0,80,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	;.db 0,90,90,0
+	;.db 0,80,80,0
 	;Lab, MinicannonS, MinicannonN, MinicannonW
-	;.db 90,0,0,0
+	;.db 80,0,0,0
 	;MinicannonE, Laser, BlackCannonS, BlackCannonN
 	;.db 0,0,0,0
 	;Volcano, Factory, Deathray, Variable Tile
@@ -23,15 +23,15 @@ TerrainFirepowerBoostRobocoPower:
 	;Null, Plains, River, Mountain
 	.db 0,0,0,0
 	;Forest, Road, City, Sea
-	.db 0,0,90,0
+	.db 0,0,80,0
 	;HQ, None, Airport, Port
-	.db 90,0,90,90
+	.db 80,0,80,80
 	;Bridge, Shoal, Base, Pipeline
-	.db 0,0,90,0
+	.db 0,0,80,0
 	;Pipeseam, Silo, Silo - Launched, Reef
-	.db 0,90,90,0
+	.db 0,80,80,0
 	;Lab, Minicannon, Tower, None
-	.db 90,0,0,0
+	.db 80,0,0,0
 	;None, Laser, BlackCannonS, BlackCannonN
 	.db 0,0,0,0
 	;Volcano, Factory, Deathray, Variable Tile
