5e20809c0849fd04814667b2fefec01db7d20c71
Zerom999 <maregee@hotmail.com>
2021-10-04 07:25:51 -0700

    Update OmegaA.asm

--- COs/AWDS/OmegaA.asm
+++ COs/AWDS/OmegaA.asm
@@ -1,1 +1,42 @@
+setCODefaults           coAWDSTowa
+	setCOGameID		coAWDSTowa,Game_AWDS
+
+	setCOGFXFullbody        coAWDSTowa,GraphicsAWDSTowaGraphic
+	setCOGFXName            coAWDSTowa,GraphicsAWDSTowaName
+	setCOGFXPalette         coAWDSTowa,GraphicsAWDSTowaPalette
+	setCOGFXFace            coAWDSTowa,GraphicsAWDSTowaFace,GraphicsAWDSTowaHappyFace,GraphicsAWDSTowaSadFace
+	setCOGFXSmallFace       coAWDSTowa,GraphicsAWDSTowaMugshot
+	setCOGFXPowerAnim       coAWDSTowa,0,0
+	setCOGFXPowerSFX        coAWDSTowa,0xb7,0xb8
+	setCOGFXSuperAnim       coAWDSTowa,7,7
+	setCOGFXSuperSFX        coAWDSTowa,0x1f7,0x1f8
+	
+	setCOName               coAWDSTowa,ScriptAWDSTowa
+	setCOpowerName          coAWDSTowa,ScriptAWDSTowa+1
+	setCOsuperName          coAWDSTowa,ScriptAWDSTowa+2
+	setCOpowerText          coAWDSTowa,ScriptAWDSTowa+8,ScriptAWDSTowa+9,ScriptAWDSTowa+10,ScriptAWDSTowa+11,ScriptAWDSTowa+12,ScriptAWDSTowa+13
+	setCOprofileText        coAWDSTowa,ScriptAWDSTowa+3,ScriptAWDSTowa+4,ScriptAWDSTowa+5,ScriptAWDSTowa+6
+	setCOvictoryText        coAWDSTowa,ScriptAWDSTowa+7
+
+	setCOFaction		coAWDSTowa,ArmyYellowComet
+	setCOMusic          	coAWDSTowa,MusicTowaTheme
+
+	setCOPowerCost          coAWDSTowa,3
+	setCOSuperCost          coAWDSTowa,6
+
+	setCOD2DSoldierStat	coAWDSTowa,20,0,0,0	
+	setCOD2DIndirectStat	coAWDSTowa,20,0,0,0
+	setCOD2DDirectStat	coAWDSTowa,-20,0,0,0
+	
+	setCOPowerSoldierStat	coAWDSTowa,40,0,0,0
+	setCOPowerIndirectStat	coAWDSTowa,40,0,0,0
+	setCOPowerSDirecttat	coAWDSTowa,-20,0,0,0
+	
+	setCOPowerSoldierStat	coAWDSTowa,60,0,0,0
+	setCOPowerIndirectStat	coAWDSTowa,60,0,0,0
+	setCOPowerSDirecttat	coAWDSTowa,-20,0,0,0
+	
+	setCOAIPowerRange       coAWDSTowa,25
+	setCOAIPowerTrigger     coAWDSTowa,95
+; blank line
 
