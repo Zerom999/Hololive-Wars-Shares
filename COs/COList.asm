b70c1ddaa6dfa100ceb1bc0e180c4053ac243798
Zerom999 <maregee@hotmail.com>
2021-10-04 07:05:34 -0700

    Update COList.asm

--- COs/COList.asm
+++ COs/COList.asm
@@ -4,19 +4,31 @@
 	;Advance Wars Dual Strike
 	
 	;Orange Star
-	.include "AWDS/Nell.asm"
-	.include "AWDS/Andy.asm"
-	.include "AWDS/Max.asm"
-	.include "AWDS/Sami.asm"
-	.include "AWDS/Hachi.asm"
-	.include "AWDS/Jake.asm"
-	.include "AWDS/Rachel.asm"
+	.include "AWDS/Sora.asm"
+	.include "AWDS/Roboco.asm"
+	.include "AWDS/Miko.asm"
+	.include "AWDS/Suisei.asm"
+	.include "AWDS/AZKi.asm"
+	.include "AWDS/Mel.asm"
+	.include "AWDS/Matsuri.asm"
+	.include "AWDS/Aki.asm"
+	.include "AWDS/Haato.asm"
+	.include "AWDS/Fubuki.asm"
+	.include "AWDS/Mio.asm"
+	.include "AWDS/Okayu.asm"
+	.include "AWDS/Korone.asm"
 	
 	;Blue Moon
-	.include "AWDS/Olaf.asm"
-	.include "AWDS/Grit.asm"
-	.include "AWDS/Colin.asm"
-	.include "AWDS/Sasha.asm"
+	.include "AWDS/Aqua.asm"
+	.include "AWDS/Shion.asm"
+	.include "AWDS/Ayame.asm"
+	.include "AWDS/Choco.asm"
+	.include "AWDS/Subaru.asm"
+	.include "AWDS/Pekora.asm"
+	.include "AWDS/Rushia.asm"
+	.include "AWDS/Flare.asm"
+	.include "AWDS/Noel.asm"
+	.include "AWDS/Marine.asm"
 	
 	;Green Earth
 	.include "AWDS/Eagle.asm"
