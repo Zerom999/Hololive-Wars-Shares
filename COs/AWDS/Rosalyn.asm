327d97b59fe86c878f5310ff770b80a142548892
Zerom999 <maregee@hotmail.com>
2021-10-04 06:49:44 -0700

    Create Rosalyn.asm

---
+++ COs/AWDS/Rosalyn.asm
@@ -0,0 +1,34 @@
+setCODefaults           coAWDSRoboco
+	setCOGameID		coAWDSRoboco,Game_AWDS
+
+	setCOGFXFullbody        coAWDSRoboco,GraphicsAWDSRobocoGraphic
+	setCOGFXName            coAWDSRoboco,GraphicsAWDSRobocoName
+	setCOGFXPalette         coAWDSRoboco,GraphicsAWDSRobocoPalette
+	setCOGFXFace            coAWDSRoboco,GraphicsAWDSRobocoFace,GraphicsAWDSRobocoHappyFace,GraphicsAWDSRobocoSadFace
+	setCOGFXSmallFace       coAWDSRoboco,GraphicsAWDSRobocoMugshot
+	setCOGFXPowerAnim       coAWDSRoboco,2,0
+	setCOGFXPowerSFX        coAWDSRoboco,0xb7,0xb8
+	setCOGFXSuperAnim       coAWDSRoboco,7,7
+	setCOGFXSuperSFX        coAWDSRoboco,0x1F4,0x1f5
+	
+	setCOName               coAWDSRoboco,ScriptAWDSRoboco
+	setCOpowerName          coAWDSRoboco,ScriptAWDSRoboco+1
+	setCOsuperName          coAWDSRoboco,ScriptAWDSRoboco+2
+	setCOpowerText          coAWDSRoboco,ScriptAWDSRoboco+8,ScriptAWDSRoboco+9,ScriptAWDSRoboco+10,ScriptAWDSRoboco+11,ScriptAWDSRoboco+12,ScriptAWDSRoboco+13
+	setCOprofileText        coAWDSRoboco,ScriptAWDSRoboco+3,ScriptAWDSRoboco+4,ScriptAWDSRoboco+5,ScriptAWDSRoboco+6
+	setCOvictoryText        coAWDSRoboco,ScriptAWDSRoboco+7
+
+	setCOFaction		coAWDSRoboco,ArmyOrangeStar
+	setCOMusic          	coAWDSRoboco,MusicRobocoTheme
+	
+	setCOPowerCost		coAWDSRoboco,5
+	setCOSuperCost		coAWDSRoboco,10
+	
+	setCOD2DTerrainFirepowerList	coAWDSRoboco,TerrainFirepowerBoostRobocoDaytoDay
+	setCOPowerTerrainFirepowerList	coAWDSRoboco,TerrainFirepowerBoostRobocoPower
+	setCOSuperTerrainFirepowerList	coAWDSRoboco,TerrainFirepowerBoostRobocoSuper
+
+	setCOAIPowerRange       	coAWDSRoboco,25
+	setCOAIPowerTrigger     	coAWDSRoboco,95
+	setCOAIPowerTriggerASM  	coAWDSRoboco,AIActivateOnProperties ;Replace with City one for COP when transplanted
+; blank line
