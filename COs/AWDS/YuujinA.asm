493d8a3861a7562c28c507d1bf523a5601302967
Zerom999 <maregee@hotmail.com>
2021-10-04 07:19:52 -0700

    Update YuujinA.asm

--- COs/AWDS/YuujinA.asm
+++ COs/AWDS/YuujinA.asm
@@ -12,23 +12,23 @@
 	setCOGFXSuperSFX        coAWDSYuujinA,0x1f7,0x1f8
 	
 	setCOName               coAWDSYuujinA,ScriptAWDSYuujinA
-	setCOpowerName          coAWDSSora,ScriptAWDSYuujinA+1
-	setCOsuperName          coAWDSSora,ScriptAWDSYuujinA+2
+	setCOpowerName          coAWDSYuujinA,ScriptAWDSYuujinA+1
+	setCOsuperName          coAWDSYuujinA,ScriptAWDSYuujinA+2
 	setCOpowerText          coAWDSSora,ScriptAWDSYuujinA+8,ScriptAWDSYuujinA+9,ScriptAWDSYuujinA+10,ScriptAWDSYuujinA+11,ScriptAWDSYuujinA+12,ScriptAWDSYuujinA+13
 	setCOprofileText        coAWDSSora,ScriptAWDSYuujinA+3,ScriptAWDSYuujinA+4,ScriptAWDSYuujinA+5,ScriptAWDSYuujinA+6
 	setCOvictoryText        coAWDSSora,ScriptAWDSYuujinA+7
 
-	setCOFaction		coAWDSSora,ArmyOrangeStar
-	setCOMusic          	coAWDSSora,MusicSoraTheme
+	setCOFaction		coAWDSYuujinA,ArmyOrangeStar
+	setCOMusic          	coAWDSYuujinA,MusicSoraTheme
 
-	setCOPowerCost          coAWDSSora,4
-	setCOSuperCost          coAWDSSora,8
+	setCOPowerCost          coAWDSYuujinA,6
+	setCOSuperCost          coAWDSSora,12
 
-	setCOPowerDirectStat	coAWDSSora,30,20,1,0	
-	setCOPowerIndirectStat	coAWDSSora,30,20,1,0
+	setCOPowerDirectStat	coAWDSYuujinA,30,20,1,0	
+	setCOPowerIndirectStat	coAWDSYuujinA,20,1,0
 	
-	setCOSuperDirectStat	coAWDSSora,60,40,2,0	
-	setCOSuperIndirectStat	coAWDSSora,60,40,2,0
+	setCOSuperDirectStat	coAWDSYuujinA,40,2,0	
+	setCOSuperIndirectStat	coAWDSYuujinA,40,2,0
 	
 	setCOAIPowerRange       coAWDSSora,25
 	setCOAIPowerTrigger     coAWDSSora,95
