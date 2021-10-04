fcda18513c9aa20153f38ef8f81fdc56a2226906
Zerom999 <maregee@hotmail.com>
2021-10-04 07:14:24 -0700

    Update YuujinA.asm

--- COs/AWDS/YuujinA.asm
+++ COs/AWDS/YuujinA.asm
@@ -1,1 +1,36 @@
+ setCODefaults           coAWDSSora
+	setCOGameID		coAWDSSora,Game_AWDS
+
+	setCOGFXFullbody        coAWDSSora,GraphicsAWDSSoraGraphic
+	setCOGFXName            coAWDSSora,GraphicsAWDSSoraName
+	setCOGFXPalette         coAWDSSora,GraphicsAWDSSoraPalette
+	setCOGFXFace            coAWDSSora,GraphicsAWDSoraFace,GraphicsAWDSSoraHappyFace,GraphicsAWDSSoraSadFace
+	setCOGFXSmallFace       coAWDSSora,GraphicsAWDSSoraMugshot
+	setCOGFXPowerAnim       coAWDSSora,0,0
+	setCOGFXPowerSFX        coAWDSSora,0xb7,0xb8
+	setCOGFXSuperAnim       coAWDSSora,7,7
+	setCOGFXSuperSFX        coAWDSSora,0x1f7,0x1f8
+	
+	setCOName               coAWDSSora,ScriptAWDSSora
+	setCOpowerName          coAWDSSora,ScriptAWDSSora+1
+	setCOsuperName          coAWDSSora,ScriptAWDSSora+2
+	setCOpowerText          coAWDSSora,ScriptAWDSSora+8,ScriptAWDSSora+9,ScriptAWDSSora+10,ScriptAWDSSoraa+11,ScriptAWDSSora+12,ScriptAWDSSora+13
+	setCOprofileText        coAWDSSora,ScriptAWDSSora+3,ScriptAWDSSora+4,ScriptAWDSSora+5,ScriptAWDSSora+6
+	setCOvictoryText        coAWDSSora,ScriptAWDSSora+7
+
+	setCOFaction		coAWDSSora,ArmyOrangeStar
+	setCOMusic          	coAWDSSora,MusicSoraTheme
+
+	setCOPowerCost          coAWDSSora,4
+	setCOSuperCost          coAWDSSora,8
+
+	setCOPowerDirectStat	coAWDSSora,30,20,1,0	
+	setCOPowerIndirectStat	coAWDSSora,30,20,1,0
+	
+	setCOSuperDirectStat	coAWDSSora,60,40,2,0	
+	setCOSuperIndirectStat	coAWDSSora,60,40,2,0
+	
+	setCOAIPowerRange       coAWDSSora,25
+	setCOAIPowerTrigger     coAWDSSora,95
+; blank line
 
